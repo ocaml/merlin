612826bcc36cbe1922825e76e53f55343c2a6423
Frederic Bour <frederic.bour@lakaban.net>
2013-01-27 16:30:06 +0100

    Changed extension format
    Split in private(fake) and public(toplevel) part.

--- extensions.ml
+++ extensions.ml
@@ -8,7 +8,8 @@ let type_sig env sg =
   let sg = Typemod.transl_signature env sg in
   sg.Typedtree.sig_type
 
-let ext_lwt = parse_sig
+let ext_lwt =
+  parse_sig
   "module Lwt : sig
     val raise_lwt' : 'a -> 'b Lwt.t
     val un_lwt : 'a Lwt.t -> 'a
@@ -17,10 +18,17 @@ let ext_lwt = parse_sig
     val finally' : 'a Lwt.t -> unit Lwt.t -> 'a Lwt.t
     val un_stream : 'a Lwt_stream.t -> 'a
     val unit_lwt : unit Lwt.t -> unit Lwt.t
-  end"
+  end",
+  parse_sig "val (>>) : unit Lwt.t -> 'a Lwt.t -> 'a Lwt.t"
 
 let registry = [ext_lwt]                       
 
 let register env =
-  let sg = List.concat (List.map (fun sg' -> try type_sig env sg' with exn -> (*FIXME:LOG ME*) []) registry) in
-  Env.add_module ident (Types.Mty_signature sg) env
+  (* Log errors ? *)
+  let try_type sg' = try type_sig env sg' with exn -> [] in
+  let fakes, tops =
+    List.split (List.map (fun (fake,top) -> try_type fake, try_type top) registry)
+  in
+  let env = Env.add_module ident (Types.Mty_signature (List.concat fakes)) env in
+  let env = Env.add_signature (List.concat tops) env in
+  env
