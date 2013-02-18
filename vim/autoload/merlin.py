5888eca4bfcd0a8e71b1b00fb303822073251413
Thomas Refis <refis.thomas@gmail.com>
2013-02-18 14:14:41 +0100

    [vim] (minor commit) replaces newlines by spaces in warning messages

--- vim/autoload/merlin.py
+++ vim/autoload/merlin.py
@@ -314,7 +314,7 @@ def vim_loclist(vimvar):
         lnum = error['start']['line']
         lcol = error['start']['col'] + 1
     vim.command("let l:tmp = {'bufnr':%d,'lnum':%d,'col':%d,'vcol':0,'nr':%d,'pattern':'','text':'%s','type':'%s','valid':1}" %
-        (bufnr, lnum, lcol, nr, error['message'].replace("'", "''"), ty))
+        (bufnr, lnum, lcol, nr, error['message'].replace("'", "''").replace("\n", " "), ty))
     nr = nr + 1
     vim.command("call add(%s, l:tmp)" % vimvar)
 
