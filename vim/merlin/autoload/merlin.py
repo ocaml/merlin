5d02a43cec20ce02d3bec694b1ac1d11f95ccb7c
Thomas Refis <refis.thomas@gmail.com>
2013-08-10 06:30:31 -0400

    vim: don't split window when 'locate' result is in the current buffer

--- vim/merlin/autoload/merlin.py
+++ vim/merlin/autoload/merlin.py
@@ -182,10 +182,12 @@ def command_locate(path, line, col):
     if not isinstance(pos_or_err, dict):
       print(pos_or_err)
     else:
+      curr_fpath = vim.current.buffer.name
       fpath = pos_or_err['file']
       l = pos_or_err['pos']['line']
       c = pos_or_err['pos']['col']
-      vim.command(":split %s" % fpath)
+      if curr_fpath != fpath:
+        vim.command(":split %s" % fpath)
       vim.current.window.cursor = (l, c)
   except MerlinExc as e:
     try_print_error(e)
