308af45bcd2f7e152ec2ecfaf8f075467f7916a7
Thomas Refis <refis.thomas@gmail.com>
2013-08-18 15:36:52 +0100

    cleanup: remove unused options from Main_args

--- src/main_args.ml
+++ src/main_args.ml
@@ -20,197 +20,37 @@ let mk_real_paths f =
     "-real-paths", Arg.Unit f, " Display real paths in types rather than short ones"
 ;;
 
-let mk_a f =
-  "-a", Arg.Unit f, " Build a library"
-;;
-
 let mk_absname f =
   "-absname", Arg.Unit f, "  Show absolute filenames in error message"
-;;
-
-let mk_annot f =
-  "-annot", Arg.Unit f, " Save information in <filename>.annot"
-;;
-
-let mk_binannot f =
-  "-bin-annot", Arg.Unit f, " Save typedtree in <filename>.cmt"
-;;
-
-let mk_c f =
-  "-c", Arg.Unit f, " Compile only (do not link)"
-;;
-
-let mk_cc f =
-  "-cc", Arg.String f, "<command>  Use <command> as the C compiler and linker"
-;;
-
-let mk_cclib f =
-  "-cclib", Arg.String f, "<opt>  Pass option <opt> to the C linker"
-;;
-
-let mk_ccopt f =
-  "-ccopt", Arg.String f, "<opt>  Pass option <opt> to the C compiler and linker"
-;;
-
-let mk_compact f =
-  "-compact", Arg.Unit f, " Optimize code size rather than speed"
 ;;
 
 let mk_config f =
   "-config", Arg.Unit f, " Print configuration values and exit"
 ;;
 
-let mk_custom f =
-  "-custom", Arg.Unit f, " Link in custom mode"
-;;
-
-let mk_dllib f =
-  "-dllib", Arg.String f, "<lib>  Use the dynamically-loaded library <lib>"
-;;
-
-let mk_dllpath f =
-  "-dllpath", Arg.String f,
-  "<dir>  Add <dir> to the run-time search path for shared libraries"
-;;
-
-let mk_dtypes f =
-  "-dtypes", Arg.Unit f, " (deprecated) same as -annot"
-;;
-
-let mk_for_pack_byt () =
-  "-for-pack", Arg.String ignore,
-  "<ident>  Ignored (for compatibility with ocamlopt)"
-;;
-
-let mk_for_pack_opt f =
-  "-for-pack", Arg.String f,
-  "<ident>  Generate code that can later be `packed' with\n\
-  \     ocamlopt -pack -o <ident>.cmx"
-;;
-
-let mk_g_byt f =
-  "-g", Arg.Unit f, " Save debugging information"
-;;
-
-let mk_g_opt f =
-  "-g", Arg.Unit f, " Record debugging information for exception backtrace"
-;;
-
-let mk_i f =
-  "-i", Arg.Unit f, " Print inferred interface"
-;;
-
 let mk_I f =
   "-I", Arg.String f, "<dir>  Add <dir> to the list of include directories"
-;;
-
-let mk_impl f =
-  "-impl", Arg.String f, "<file>  Compile <file> as a .ml file"
 ;;
 
 let mk_init f =
   "-init", Arg.String f, "<file>  Load <file> instead of default init file"
 ;;
 
-let mk_inline f =
-  "-inline", Arg.Int f, "<n>  Set aggressiveness of inlining to <n>"
-;;
-
-let mk_intf f =
-  "-intf", Arg.String f, "<file>  Compile <file> as a .mli file"
-;;
-
-let mk_intf_suffix f =
-  "-intf-suffix", Arg.String f,
-  "<string>  Suffix for interface files (default: .mli)"
-;;
-
-let mk_intf_suffix_2 f =
-  "-intf_suffix", Arg.String f, "<string>  (deprecated) same as -intf-suffix"
-;;
-
 let mk_labels f =
   "-labels", Arg.Unit f, " Use commuting label mode"
-;;
-
-let mk_linkall f =
-  "-linkall", Arg.Unit f, " Link all modules, even unused ones"
-;;
-
-let mk_make_runtime f =
-  "-make-runtime", Arg.Unit f,
-  " Build a runtime system with given C objects and libraries"
-;;
-
-let mk_make_runtime_2 f =
-  "-make_runtime", Arg.Unit f, " (deprecated) same as -make-runtime"
-;;
-
-let mk_modern f =
-  "-modern", Arg.Unit f, " (deprecated) same as -labels"
 ;;
 
 let mk_no_app_funct f =
   "-no-app-funct", Arg.Unit f, " Deactivate applicative functors"
 ;;
 
-let mk_noassert f =
-  "-noassert", Arg.Unit f, " Do not compile assertion checks"
-;;
-
-let mk_noautolink_byt f =
-  "-noautolink", Arg.Unit f,
-  " Do not automatically link C libraries specified in .cma files"
-;;
-
-let mk_noautolink_opt f =
-  "-noautolink", Arg.Unit f,
-  " Do not automatically link C libraries specified in .cmxa files"
-;;
-
-let mk_nodynlink f =
-  "-nodynlink", Arg.Unit f,
-  " Enable optimizations for code that will not be dynlinked"
-;;
-
 let mk_nolabels f =
   "-nolabels", Arg.Unit f, " Ignore non-optional labels in types"
-;;
-
-let mk_noprompt f =
-  "-noprompt", Arg.Unit f, " Suppress all prompts"
-;;
-
-let mk_nopromptcont f =
-  "-nopromptcont", Arg.Unit f,
-  " Suppress prompts for continuation lines of multi-line inputs"
 ;;
 
 let mk_nostdlib f =
   "-nostdlib", Arg.Unit f,
   " Do not add default directory to the list of include directories"
-;;
-
-let mk_o f =
-  "-o", Arg.String f, "<file>  Set output file name to <file>"
-;;
-
-let mk_output_obj f =
-  "-output-obj", Arg.Unit f, " Output a C object file instead of an executable"
-;;
-
-let mk_p f =
-  "-p", Arg.Unit f,
-  " Compile and link with profiling support for \"gprof\"\n\
-  \     (not supported on all platforms)"
-;;
-
-let mk_pack_byt f =
-  "-pack", Arg.Unit f, " Package the given .cmo files into one .cmo"
-;;
-
-let mk_pack_opt f =
-  "-pack", Arg.Unit f, " Package the given .cmx files into one .cmx"
 ;;
 
 let mk_pp f =
@@ -225,15 +65,6 @@ let mk_rectypes f =
   "-rectypes", Arg.Unit f, " Allow arbitrary recursive types"
 ;;
 
-let mk_runtime_variant f =
-  "-runtime-variant", Arg.String f,
-  "<str>  Use the <str> variant of the run-time system"
-;;
-
-let mk_S f =
-  "-S", Arg.Unit f, " Keep intermediate assembly file"
-;;
-
 let mk_stdin f =
   "-stdin", Arg.Unit f, " Read script from standard input"
 ;;
@@ -241,30 +72,6 @@ let mk_stdin f =
 let mk_strict_sequence f =
   "-strict-sequence", Arg.Unit f,
   " Left-hand part of a sequence must have type unit"
-;;
-
-let mk_shared f =
-  "-shared", Arg.Unit f, " Produce a dynlinkable plugin"
-;;
-
-let mk_thread f =
-  "-thread", Arg.Unit f,
-  " Generate code that supports the system threads library"
-;;
-
-let mk_unsafe f =
-  "-unsafe", Arg.Unit f,
-  " Do not compile bounds checking on array and string access"
-;;
-
-let mk_use_runtime f =
-  "-use-runtime", Arg.String f,
-  "<file>  Generate bytecode for the given runtime system"
-;;
-
-let mk_use_runtime_2 f =
-  "-use_runtime", Arg.String f,
-  "<file>  (deprecated) same as -use-runtime"
 ;;
 
 let mk_v f =
@@ -278,16 +85,6 @@ let mk_version f =
 
 let mk_vnum f =
   "-vnum", Arg.Unit f, " Print version number and exit"
-;;
-
-let mk_verbose f =
-  "-verbose", Arg.Unit f, " Print calls to external commands"
-;;
-
-let mk_vmthread f =
-  "-vmthread", Arg.Unit f,
-  " Generate code that supports the threads library with VM-level\n\
-  \     scheduling"
 ;;
 
 let mk_w f =
@@ -330,78 +127,6 @@ let mk_nopervasives f =
 
 let mk_use_prims f =
   "-use-prims", Arg.String f, "<file>  (undocumented)"
-;;
-
-let mk_dparsetree f =
-  "-dparsetree", Arg.Unit f, " (undocumented)"
-;;
-
-let mk_drawlambda f =
-  "-drawlambda", Arg.Unit f, " (undocumented)"
-;;
-
-let mk_dlambda f =
-  "-dlambda", Arg.Unit f, " (undocumented)"
-;;
-
-let mk_dclambda f =
-  "-dclambda", Arg.Unit f, " (undocumented)"
-;;
-
-let mk_dinstr f =
-  "-dinstr", Arg.Unit f, " (undocumented)"
-;;
-
-let mk_dcmm f =
-  "-dcmm", Arg.Unit f, " (undocumented)"
-;;
-
-let mk_dsel f =
-  "-dsel", Arg.Unit f, " (undocumented)"
-;;
-
-let mk_dcombine f =
-  "-dcombine", Arg.Unit f, " (undocumented)"
-;;
-
-let mk_dlive f =
-  "-dlive", Arg.Unit f, " (undocumented)"
-;;
-
-let mk_dspill f =
-  "-dspill", Arg.Unit f, " (undocumented)"
-;;
-
-let mk_dsplit f =
-  "-dsplit", Arg.Unit f, " (undocumented)"
-;;
-
-let mk_dinterf f =
-  "-dinterf", Arg.Unit f, " (undocumented)"
-;;
-
-let mk_dprefer f =
-  "-dprefer", Arg.Unit f, " (undocumented)"
-;;
-
-let mk_dalloc f =
-  "-dalloc", Arg.Unit f, " (undocumented)"
-;;
-
-let mk_dreload f =
-  "-dreload", Arg.Unit f, " (undocumented)"
-;;
-
-let mk_dscheduling f =
-  "-dscheduling", Arg.Unit f, " (undocumented)"
-;;
-
-let mk_dlinear f =
-  "-dlinear", Arg.Unit f, " (undocumented)"
-;;
-
-let mk_dstartup f =
-  "-dstartup", Arg.Unit f, " (undocumented)"
 ;;
 
 let mk__ f =
@@ -456,27 +181,18 @@ struct
     mk_init F._init;
     mk_labels F._labels;
     mk_no_app_funct F._no_app_funct;
-    mk_noassert F._noassert;
     mk_nolabels F._nolabels;
-    mk_noprompt F._noprompt;
-    mk_nopromptcont F._nopromptcont;
     mk_nostdlib F._nostdlib;
     mk_principal F._principal;
     mk_rectypes F._rectypes;
     mk_stdin F._stdin;
     mk_strict_sequence F._strict_sequence;
-    mk_unsafe F._unsafe;
     mk_version F._version;
     mk_vnum F._vnum;
     mk_w F._w;
     mk_warn_error F._warn_error;
     mk_warn_help F._warn_help;
     mk_protocol F._protocol;
-
-    mk_dparsetree F._dparsetree;
-    mk_drawlambda F._drawlambda;
-    mk_dlambda F._dlambda;
-    mk_dinstr F._dinstr;
 
     mk__ F.anonymous;
   ]
