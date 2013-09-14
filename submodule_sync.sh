0da21e1bbcc136db3ac06d95458962603b9db32c
Frederic Bour <frederic.bour@lakaban.net>
2013-09-14 23:39:53 +0100

    configure: Select git version of typers if available (submodule_sync.sh updated)

--- submodule_sync.sh
+++ submodule_sync.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-git submodule sync
-rm -rf vim/vimbufsync
-cp -R vim/vimbufsync.git vim/vimbufsync
-rm -rf vim/vimbufsync/.git
+SUBMODULES="vim/vimbufsync src/ocaml_400 src/ocaml_401"
+
+git submodule update
+for i in $SUBMODULES; do
+  [ -e "$i".git/.git ] || {
+    echo "Uninitialized submodule '$i', skipping (you may need to run 'git submodule init')."
+    continue
+  }
+  echo "Updating '$i'."
+  rm -rf "$i"
+  cp -R "$i".git "$i"
+  rm -rf "$i"/.git
+done
