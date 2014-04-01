c0ff48f031473a9a1f96e1fa9320f7541e4eab46
Frederic Bour <frederic.bour@lakaban.net>
2014-04-01 15:32:49 +0100

    Import ocaml_402 source code

---
+++ src/ocaml/error_report.ml
@@ -0,0 +1,114 @@
+(* {{{ COPYING *(
+
+  This file is part of Merlin, an helper for ocaml editors
+
+  Copyright (C) 2013  Frédéric Bour  <frederic.bour(_)lakaban.net>
+                      Thomas Refis  <refis.thomas(_)gmail.com>
+                      Simon Castellan  <simon.castellan(_)iuwt.fr>
+
+  Permission is hereby granted, free of charge, to any person obtaining a
+  copy of this software and associated documentation files (the "Software"),
+  to deal in the Software without restriction, including without limitation the
+  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+  sell copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  The Software is provided "as is", without warranty of any kind, express or
+  implied, including but not limited to the warranties of merchantability,
+  fitness for a particular purpose and noninfringement. In no event shall
+  the authors or copyright holders be liable for any claim, damages or other
+  liability, whether in an action of contract, tort or otherwise, arising
+  from, out of or in connection with the software or the use or other dealings
+  in the Software.
+
+)* }}} *)
+
+open Std
+
+type t = {valid: bool; loc: Location.t; text: string; where:string}
+
+let format ~valid ~where ?(loc=Location.none) text =
+  loc, {valid; loc; text; where}
+
+
+let strict_of_exn = function
+  | Typecore.Error (loc, env, e) ->
+    let ppf, to_string = Format.to_string () in
+    Typecore.report_error env ppf e;
+    Some (format ~valid:true ~where:"type" ~loc (to_string ()))
+  | Typetexp.Error (loc, env, e) ->
+    let ppf, to_string = Format.to_string () in
+    Typetexp.report_error env ppf e;
+    Some (format ~valid:true ~where:"type" ~loc (to_string ()))
+  | Typedecl.Error (loc, e) ->
+    let ppf, to_string = Format.to_string () in
+    Typedecl.report_error ppf e;
+    Some (format ~valid:true ~where:"type" ~loc (to_string ()))
+  | Typemod.Error (loc, env, e) ->
+    let ppf, to_string = Format.to_string () in
+    Typemod.report_error env ppf e;
+    Some (format ~valid:true ~where:"type" ~loc (to_string ()))
+  | Typeclass.Error (loc, env, e) ->
+    let ppf, to_string = Format.to_string () in
+    Typeclass.report_error env ppf e;
+    Some (format ~valid:true ~where:"type" ~loc (to_string ()))
+  | Env.Error e ->
+    let ppf, to_string = Format.to_string () in
+    Env.report_error ppf e;
+    Some (format ~valid:true ~where:"env" (to_string ()))
+  | Syntaxerr.Escape_error pos ->
+    Some (format ~valid:true ~where:"parser"
+            ~loc:{Location. loc_start = pos; loc_end = pos; loc_ghost = true}
+            "Syntax error")
+  | Syntaxerr.Error e ->
+    let ppf, to_string = Format.to_string () in
+    Syntaxerr.report_error ppf e;
+    let loc = match e with
+      | Syntaxerr.Unclosed (loc,_,loc',_) ->
+          Location.({ loc_start = loc.loc_start;
+                      loc_end = loc'.loc_end;
+                      loc_ghost = false;
+                    })
+      | Syntaxerr.Applicative_path loc -> loc
+      | Syntaxerr.Variable_in_scope (loc,_) -> loc
+      | Syntaxerr.Other loc -> loc
+      | Syntaxerr.Expecting (loc,_) -> loc
+    in
+    Some (format ~valid:true ~where:"parser" ~loc (to_string ()))
+  | Parsing_aux.Warning (loc, msg) ->
+    Some (format ~valid:true ~where:"warning" ~loc msg)
+  | Raw_parser.Error ->
+    Some (format ~valid:false ~where:"parser" "Parse error")
+  | Findlib.No_such_package (pkg,msg) ->
+    Some (format ~valid:true ~where:"env" (Printf.sprintf "Package not found %S (%s)" pkg msg))
+  (*| Outline.Malformed_module (_,loc) ->
+    Some (format ~valid:true ~where:"parser" ~loc "Malformed module")*)
+  | Error_classifier.Error c ->
+    let loc = Error_classifier.loc c in
+    let msg = Error_classifier.classify c in
+    Some (format ~valid:true ~where:"parser" ~loc msg)
+  | exn -> None
+
+let null_loc =
+  let z = {Lexing. pos_fname = ""; pos_bol = 0; pos_lnum = 1; pos_cnum = 0} in
+  {Location. loc_start = z; loc_end = z; loc_ghost = true}
+
+let of_exn exn = match strict_of_exn exn with
+  | Some j -> j
+  | None -> format ~valid:false ~where:"unknown" ~loc:null_loc
+              (Printexc.to_string exn)
+
+let strict_of_exns list =
+  List.sort (fun (l1,_) (l2,_) ->
+      Location.(Lexing.compare_pos l1.loc_start l2.loc_start))
+    (List.filter_map strict_of_exn list)
+
+let of_exns list =
+  List.sort (fun (l1,_) (l2,_) ->
+      Location.(Lexing.compare_pos l1.loc_start l2.loc_start))
+    (List.map of_exn list)
+
+let error_catcher = strict_of_exn
---
+++ src/ocaml/parsing/ast_mapper.mli
@@ -0,0 +1,123 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                                OCaml                                *)
+(*                                                                     *)
+(*                        Alain Frisch, LexiFi                         *)
+(*                                                                     *)
+(*  Copyright 2012 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the Q Public License version 1.0.               *)
+(*                                                                     *)
+(***********************************************************************)
+
+(** Helpers to write Parsetree rewriters *)
+
+open Parsetree
+
+(** {2 A generic Parsetree mapper} *)
+
+type mapper = {
+  attribute: mapper -> attribute -> attribute;
+  attributes: mapper -> attribute list -> attribute list;
+  case: mapper -> case -> case;
+  cases: mapper -> case list -> case list;
+  class_declaration: mapper -> class_declaration -> class_declaration;
+  class_description: mapper -> class_description -> class_description;
+  class_expr: mapper -> class_expr -> class_expr;
+  class_field: mapper -> class_field -> class_field;
+  class_signature: mapper -> class_signature -> class_signature;
+  class_structure: mapper -> class_structure -> class_structure;
+  class_type: mapper -> class_type -> class_type;
+  class_type_declaration: mapper -> class_type_declaration
+                          -> class_type_declaration;
+  class_type_field: mapper -> class_type_field -> class_type_field;
+  constructor_declaration: mapper -> constructor_declaration
+                           -> constructor_declaration;
+  expr: mapper -> expression -> expression;
+  extension: mapper -> extension -> extension;
+  extension_constructor: mapper -> extension_constructor
+                         -> extension_constructor;
+  include_declaration: mapper -> include_declaration -> include_declaration;
+  include_description: mapper -> include_description -> include_description;
+  label_declaration: mapper -> label_declaration -> label_declaration;
+  location: mapper -> Location.t -> Location.t;
+  module_binding: mapper -> module_binding -> module_binding;
+  module_declaration: mapper -> module_declaration -> module_declaration;
+  module_expr: mapper -> module_expr -> module_expr;
+  module_type: mapper -> module_type -> module_type;
+  module_type_declaration: mapper -> module_type_declaration
+                           -> module_type_declaration;
+  open_description: mapper -> open_description -> open_description;
+  pat: mapper -> pattern -> pattern;
+  payload: mapper -> payload -> payload;
+  signature: mapper -> signature -> signature;
+  signature_item: mapper -> signature_item -> signature_item;
+  structure: mapper -> structure -> structure;
+  structure_item: mapper -> structure_item -> structure_item;
+  typ: mapper -> core_type -> core_type;
+  type_declaration: mapper -> type_declaration -> type_declaration;
+  type_extension: mapper -> type_extension -> type_extension;
+  type_kind: mapper -> type_kind -> type_kind;
+  value_binding: mapper -> value_binding -> value_binding;
+  value_description: mapper -> value_description -> value_description;
+  with_constraint: mapper -> with_constraint -> with_constraint;
+}
+(** A mapper record implements one "method" per syntactic category,
+    using an open recursion style: each method takes as its first
+    argument the mapper to be applied to children in the syntax
+    tree. *)
+
+val default_mapper: mapper
+(** A default mapper, which implements a "deep identity" mapping. *)
+
+(** {2 Apply mappers to compilation units} *)
+
+val apply: source:string -> target:string -> mapper -> unit
+(** Apply a mapper (parametrized by the unit name) to a dumped
+    parsetree found in the [source] file and put the result in the
+    [target] file. The [structure] or [signature] field of the mapper
+    is applied to the implementation or interface.  *)
+
+val run_main: (string list -> mapper) -> unit
+(** Entry point to call to implement a standalone -ppx rewriter from a
+    mapper, parametrized by the command line arguments.  The current
+    unit name can be obtained from [Location.input_name].  This
+    function implements proper error reporting for uncaught
+    exceptions. *)
+
+(** {2 Registration API} *)
+
+val register_function: (string -> (string list -> mapper) -> unit) ref
+
+val register: string -> (string list -> mapper) -> unit
+
+(** Apply the [register_function].  The default behavior is to run the
+    mapper immediately, taking arguments from the process command
+    line.  This is to support a scenario where a mapper is linked as a
+    stand-alone executable.
+
+    It is possible to overwrite the [register_function] to define
+    "-ppx drivers", which combine several mappers in a single process.
+    Typically, a driver starts by defining [register_function] to a
+    custom implementation, then lets ppx rewriters (linked statically
+    or dynamically) register themselves, and then run all or some of
+    them.  It is also possible to have -ppx drivers apply rewriters to
+    only specific parts of an AST.
+
+    The first argument to [register] is a symbolic name to be used by
+    the ppx driver.  *)
+
+
+(** {2 Convenience functions to write mappers} *)
+
+val map_opt: ('a -> 'b) -> 'a option -> 'b option
+
+val extension_of_error: Location.error -> extension
+(** Encode an error into an 'ocaml.error' extension node which can be
+    inserted in a generated Parsetree.  The compiler will be
+    responsible for reporting the error. *)
+
+val attribute_of_warning: Location.t -> string -> attribute
+(** Encode a warning message into an 'ocaml.ppwarning' attribute which can be
+    inserted in a generated Parsetree.  The compiler will be
+    responsible for reporting the warning. *)
