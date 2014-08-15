f25dcc5832c0b0e4856ee638b73cca877dfaef47
Frédéric Bour <frederic.bour@lakaban.net>
2014-08-15 18:47:37 +0100

    Tweak selection of lexer for reconstruction

--- src/frontend/command.ml
+++ src/frontend/command.ml
@@ -169,7 +169,7 @@ let dispatch (state : state) =
       | None ->
         let lexer = Buffer.lexer state.buffer in
         let lexer = History.seek_backward (fun (_,item) ->
-            Lexing.compare_pos pos (Lexer.item_start item) < 0)
+            Lexing.compare_pos pos (Lexer.item_start item) <= 0)
             lexer in
         begin match Lexer.reconstruct_identifier lexer with
           | [] -> []
@@ -283,7 +283,7 @@ let dispatch (state : state) =
       | None, Some pos ->
         let lexer = Buffer.lexer state.buffer in
         let lexer = History.seek_backward (fun (_,item) ->
-            Lexing.compare_pos pos (Lexer.item_start item) < 0)
+            Lexing.compare_pos pos (Lexer.item_start item) <= 0)
             lexer in
         let path = Lexer.reconstruct_identifier lexer in
         let path = List.map ~f:(fun {Location. txt} -> txt) path in
