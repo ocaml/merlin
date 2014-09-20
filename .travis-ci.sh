98f24eb0a4db39a889614bf1e14511335627fa1c
Frederic Bour <frederic.bour@lakaban.net>
2014-09-20 17:13:51 +0200

    Update travis scripts

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,10 +1,9 @@
 OPAM_DEPENDS="yojson ocamlfind"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
-4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
 4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
 4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+4.02.0,1.1.0) ppa=avsm/ocaml42+opam11 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
