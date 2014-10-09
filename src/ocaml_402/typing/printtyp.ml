a1f0f4ac35d9e19d4305a9bd6d75eaca3a71e380
Frédéric Bour <frederic.bour@lakaban.net>
2014-10-09 13:08:17 +0100

    Printtyp: short-paths stack overflow can still be triggered

--- src/ocaml_402/typing/printtyp.ml
+++ src/ocaml_402/typing/printtyp.ml
@@ -254,6 +254,7 @@ let rec normalize_type_path ?(cache=false) env p =
         let tyl = List.map repr tyl in
         if List.length params = List.length tyl
         && List.for_all2 (==) params tyl
+        && p != p1
         then normalize_type_path ~cache env p1
         else if cache || List.length params <= List.length tyl
              || not (uniq tyl) then (p, Id)
