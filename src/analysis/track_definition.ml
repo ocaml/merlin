ocaml/merlin

commit 5ecd37e9737299ed9b2d474c4756dd34d5be76a1

locate: fix weird behavior when starting point is on an alias.
Examples:

        let x = [x]
        module M = [M]

with [ ] representing the cursor, locate wouldn't go to the original definition
of x (resp. M) but 3 chars before where the alias is introduced.

src/analysis/track_definition.ml
@@ -203,14 +203,19 @@ type result = [
 
 (** Reverse the list of items − we want to start from the bottom of
     the file − and remove top level indirections. *)
-let get_top_items browsable =
+let get_top_items ?pos browsable =
+  let don't_discard x =
+    match pos with
+    | None -> true
+    | Some pos -> x.BrowseT.t_loc.Location.loc_start < pos
+  in
   List.concat_map (fun bt ->
     let open BrowseT in
     match bt.t_node with
     | Signature _
-    | Structure _ -> List.rev (Lazy.force bt.t_children)
+    | Structure _ -> List.rev_filter (Lazy.force bt.t_children) ~f:don't_discard
     | Signature_item _
-    | Structure_item _-> [ bt ]
+    | Structure_item _ when don't_discard bt -> [ bt ]
     | _ -> []
   ) browsable
 
@@ -393,7 +398,7 @@ let recover () =
   | `ML  loc -> finalize true loc
   | `MLI loc -> finalize false loc
 
-let from_string ~project ~env ~local_defs ~is_implementation ml_or_mli path =
+let from_string ~project ~env ~local_defs ~is_implementation ?pos ml_or_mli path =
   File_switching.reset () ;
   cwd := "" (* Reset the cwd before doing anything *) ;
   sources_path := Project.source_path project ;
@@ -442,8 +447,11 @@ let from_string ~project ~env ~local_defs ~is_implementation ml_or_mli path =
     if not (is_ghost loc) then
       `Found (None, loc.Location.loc_start)
     else
+      let () = debug_log
+        "present in the environment, but ghost lock. walking up the typedtree."
+      in
       let modules = path_to_list path' in
-      let items   = get_top_items (Browse.of_typer_contents local_defs) in
+      let items   = get_top_items ?pos (Browse.of_typer_contents local_defs) in
       match check_item ~source:is_implementation modules items with
       | `Not_found when Fallback.is_set () -> recover ()
       | `Not_found -> `Not_found (path, File_switching.where_am_i ())

src/analysis/track_definition.mli
@@ -5,6 +5,7 @@ val from_string
   -> env:Env.t
   -> local_defs:[`Str of Typedtree.structure | `Sg of Typedtree.signature] list
   -> is_implementation:bool
+  -> ?pos:Lexing.position
   -> [ `ML | `MLI ]
   -> string
   -> [> `File_not_found of string

src/frontend/command.ml
@@ -330,7 +330,7 @@ let dispatch (state : state) =
     let project = Buffer.project state.buffer in
     begin match
       Track_definition.from_string ~project ~env ~local_defs ~is_implementation
-        ml_or_mli path
+        ?pos:opt_pos ml_or_mli path
     with
     | `Found (file, pos) ->
       Logger.info (Track_definition.section)

src/utils/std.ml
@@ -29,6 +29,13 @@ module List = struct
       let init = f x init in
       init :: (scan_left ~f xs ~init)
 
+  let rev_filter ~f lst =
+    let rec aux acc = function
+      | [] -> acc
+      | x :: xs -> aux (if f x then x :: acc else acc) xs
+    in
+    aux [] lst
+
   let rec filter_map ~f = function
     | [] -> []
     | x :: xs ->

commit 87ef36591f6f15026b52e4f6b75d91e72ec02328

locate: use Lexing.compare_pos to compare positions. Fixes bug introduced in 5ecd37e9737299ed9b2d474c4756dd34d5be76a1

src/analysis/track_definition.ml
@@ -229,13 +229,15 @@ type result = [
   | `Not_found
 ]
 
-(** Reverse the list of items − we want to start from the bottom of
-    the file − and remove top level indirections. *)
+(* Remove top level indirections (i.e. Structure and Signature) and reverse
+   their children so we start from the bottom of the file.
+   We also remove everything appearing after [pos]: we don't want to consider
+   things declared after the use point of what we are looking for. *)
 let get_top_items ?pos browsable =
   let don't_discard x =
     match pos with
     | None -> true
-    | Some pos -> x.BrowseT.t_loc.Location.loc_start < pos
+    | Some pos -> Lexing.compare_pos x.BrowseT.t_loc.Location.loc_start pos < 0
   in
   List.concat_map (fun bt ->
     let open BrowseT in
@@ -330,7 +332,10 @@ and browse_cmts ~root modules =
     | Interface intf -> `Sg intf, false
     | Implementation impl -> `Str impl, true
     | Packed (_, files) -> `Pack files, true
-    | _ -> `Not_found, true (* TODO? *)
+    | _ ->
+      (* We could try to work with partial cmt files, but it'd probably fail
+       * most of the time so... *)
+      `Not_found, true
   with
   | `Not_found, _ -> `Not_found
   | (`Str _ | `Sg _ as typedtree), source ->
@@ -398,7 +403,8 @@ and resolve_mod_alias ~source node path rest =
     resolve_mod_alias ~source (BrowseT.Module_type mod_type) path rest
   | `Mod_expr mod_expr ->
     resolve_mod_alias ~source (BrowseT.Module_expr mod_expr) path rest
-  | `Unpack -> (* FIXME: should we do something or stop here? *)
+  | `Unpack ->
+    (* FIXME: should we do something or stop here? *)
     info_log "found Tmod_unpack, expect random results." ;
     check_item ~source path rest
 