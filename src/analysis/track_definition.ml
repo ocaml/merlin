38c306df0b27457a3ba32e7c223f06f6a7ad8569
Thomas Refis <thomas.refis@gmail.com>
2015-02-02 10:40:03 +0000

    Revert "locate: follow module aliases" This reverts commit 10ef16b012fb93c5c9dbda067320ab8e46ec0394.
    Because it's actually more confusing than anything to follow aliases when
    looking for a module definition.

--- src/analysis/track_definition.ml
+++ src/analysis/track_definition.ml
@@ -40,8 +40,6 @@ let loadpath     = Fluid.from (Misc.Path_list.of_list [])
 
 let last_location = Fluid.from Location.none
 
-let target_is_module = ref false
-
 let erase_loadpath ~cwd ~new_path k =
   let str_path_list =
     List.map new_path ~f:(function
@@ -346,32 +344,25 @@ let repack = function
   | `Mod_expr me  -> Some (BrowseT.Module_expr me)
   | `Mod_type mty -> Some (BrowseT.Module_type mty)
 
-let find_mod_expr ~name str_item =
-  let mbs = Raw_compat.expose_module_binding str_item in
-  let mb = List.find ~f:(fun mb -> Ident.name mb.Typedtree.mb_id = name) mbs in
-  mb.Typedtree.mb_expr
-
-let find_mod_type ~name sig_item =
-  let mds = Raw_compat.expose_module_declaration sig_item in
-  let md = List.find ~f:(fun md -> Ident.name md.Typedtree.md_id = name) mds in
-  md.Typedtree.md_type
-
 let get_on_track ~name item =
   match
+    let open Raw_compat in
     match item.BrowseT.t_node with
     | BrowseT.Structure_item item ->
-      repack (Raw_compat.get_mod_expr_if_included ~name item),
+      repack (get_mod_expr_if_included ~name item),
       begin try
-        let me = find_mod_expr ~name item in
+        let mbs = expose_module_binding item in
+        let mb = List.find ~f:(fun mb -> Ident.name mb.Typedtree.mb_id = name) mbs in
         info_log "(get_on_track) %s is bound" name ;
-        `Direct (BrowseT.Module_expr me)
+        `Direct (BrowseT.Module_expr mb.Typedtree.mb_expr)
       with Not_found -> `Not_found end
     | BrowseT.Signature_item item ->
-      repack (Raw_compat.get_mod_type_if_included ~name item),
+      repack (get_mod_type_if_included ~name item),
       begin try
-        let mty = find_mod_type ~name item in
+        let mds = expose_module_declaration item in
+        let md = List.find ~f:(fun md -> Ident.name md.Typedtree.md_id = name) mds in
         info_log "(get_on_track) %s is bound" name ;
-        `Direct (BrowseT.Module_type mty)
+        `Direct (BrowseT.Module_type md.Typedtree.md_type)
       with Not_found -> `Not_found end
     | _ -> assert false
   with
@@ -417,37 +408,22 @@ let rec check_item ~source modules =
     | Some (path, items) -> check_item ~source path items
   in
   let get_loc ~name item rest =
-    let tagged_item, ident_locs, is_included =
+    let ident_locs, is_included =
       let open Raw_compat in
       match item.BrowseT.t_node with
       | BrowseT.Structure_item item ->
-        `Str item, str_ident_locs item, get_mod_expr_if_included item
+        str_ident_locs item, get_mod_expr_if_included item
       | BrowseT.Signature_item item ->
-        `Sig item, sig_ident_locs item, get_mod_type_if_included item
+        sig_ident_locs item, get_mod_type_if_included item
       | _ -> assert false
     in
     try
-      let loc = List.assoc name ident_locs in
-      if not !target_is_module then (
-        Logger.debugf section (fun fmt loc ->
-          Format.pp_print_string fmt "[get_loc] found at " ;
-          Location.print_loc fmt loc
-        ) loc ;
-        if source then `ML loc else `MLI loc
-      ) else (
-        match
-          match tagged_item with
-          | `Str item -> Raw_compat.remove_indir_me @@ find_mod_expr ~name item
-          | `Sig item -> Raw_compat.remove_indir_mty @@ find_mod_type ~name item
-        with
-        | `Alias target ->
-          File_switching.allow_movement () ;
-          check_item ~source (Path.to_string_list target) rest
-        | _ ->
-          debug_log "We found the module we were looking for, it's not an alias\
-                     , stopping there." ;
-          if source then `ML loc else `MLI loc
-      )
+      let res = List.assoc name ident_locs in
+      Logger.debugf section (fun fmt loc ->
+        Format.pp_print_string fmt "[get_loc] found at " ;
+        Location.print_loc fmt loc
+      ) res ;
+      if source then `ML res else `MLI res
     with Not_found ->
       match repack (is_included ~name) with
       | None ->
@@ -682,7 +658,6 @@ let namespaces = function
 exception Found of (Path.t * Location.t)
 
 let lookup ctxt ident env =
-  target_is_module := false ;
   try
     List.iter (namespaces ctxt) ~f:(fun namespace ->
       try
@@ -694,12 +669,10 @@ let lookup ctxt ident env =
         | `Mod ->
           info_log "lookup in module namespace" ;
           let path, _ = Raw_compat.lookup_module ident env in
-          target_is_module := true ;
           raise (Found (path, Location.symbol_gloc ()))
         | `Modtype ->
           info_log "lookup in module type namespace" ;
           let path, _ = Raw_compat.lookup_modtype ident env in
-          target_is_module := true ;
           raise (Found (path, Location.symbol_gloc ()))
         | `Type ->
           info_log "lookup in type namespace" ;
