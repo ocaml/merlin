6c23f666c2a6c4836aa26f24cd5ae3c0b2f02bf1
Thomas Refis <thomas.refis@gmail.com>
2015-03-05 09:04:13 +0000

    trunk: merge typedecl

--- src/ocaml_trunk/typing/typedecl.ml
+++ src/ocaml_trunk/typing/typedecl.ml
@@ -514,7 +514,7 @@ let check_well_founded env loc path to_check ty =
     with
     | Ctype.Cannot_expand ->
         let nodes =
-          if !Clflags.recursive_types && Ctype.is_contractive env ty
+          if Clflags.recursive_types () && Ctype.is_contractive env ty
           || match ty.desc with Tobject _ | Tvariant _ -> true | _ -> false
           then TypeSet.empty
           else exp_nodes in
@@ -1099,6 +1099,12 @@ let transl_type_decl env sdecl_list =
       )
       sdecl_list
   in
+  let decls  = List.map (fun (id,n) -> Fake.Nonrec.ident_drop id,n) decls in
+  let tdecls = List.map (fun ({typ_id; typ_name} as tdecl) ->
+      {tdecl with
+       typ_id = Fake.Nonrec.ident_drop typ_id;
+       typ_name = {typ_name with txt = Fake.Nonrec.drop typ_name.txt}}) tdecls
+  in
   let final_decls, final_env =
     compute_variance_fixpoint env decls required (List.map init_variance decls)
   in
@@ -1357,7 +1363,7 @@ let transl_value_decl env loc valdecl =
       let prim = Primitive.parse_declaration arity decl in
       if arity = 0 && (prim.prim_name = "" || prim.prim_name.[0] <> '%') then
         raise(Error(valdecl.pval_type.ptyp_loc, Null_arity_external));
-      if !Clflags.native_code
+      if Clflags.native_code ()
       && prim.prim_arity > 5
       && prim.prim_native_name = ""
       then raise(Error(valdecl.pval_type.ptyp_loc, Missing_native_external));
