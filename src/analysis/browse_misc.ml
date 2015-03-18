ocaml/merlin

commit a6436dc800b3f4df8c2ed86b2a42e56199db2076

completion: avoid printing redundant information (fix #279)
The description of a completion item is now:
- a type instead of a value declaration, if the kind is Value
- a type instead of "name : type", if the kind is Constructor
- unchanged otherwise

This commit also cleans up the emacs mode which post-processed the string
corresponding to the description to obtain the same result.
A similar changes has been introduced for vim in the completion-next branch,
this can be removed when rebasing the branch.

src/analysis/completion.ml
@@ -159,15 +159,9 @@ let completion_format ~exact name ?path ty =
   let kind =
     match ty with
     | `Value v ->
-      let v = (*if exact
-                 then Types.({v with val_type = verbose_type env v.val_type})
-                 else*) v
-      in
-      Printtyp.value_description ident ppf v;
+      Printtyp.type_expr ppf v.Types.val_type;
       `Value
     | `Cons c  ->
-      Format.pp_print_string ppf name;
-      Format.pp_print_string ppf " : ";
       Browse_misc.print_constructor ppf c;
       `Constructor
     | `Label label_descr ->
@@ -175,8 +169,6 @@ let completion_format ~exact name ?path ty =
         Types.(Tarrow (Raw_compat.Parsetree.arg_label_of_str "",
                        label_descr.lbl_res, label_descr.lbl_arg, Cok))
       in
-      Format.pp_print_string ppf name;
-      Format.pp_print_string ppf " : ";
       Printtyp.type_scheme ppf (Btype.newgenty desc);
       `Label
     | `Mod m   ->

emacs/merlin.el
@@ -1106,14 +1106,11 @@ errors in the fringe.  If VIEW-ERRORS-P is non-nil, display a count of them."
 
 (defun merlin-completion-format-entry (entry)
   "Format the completion entry ENTRY."
-  (let ((type
-         (cond
-          ((member (cdr (assoc 'kind entry)) '("Module" "module"))
-           " <module>")
-          ((string-equal (cdr (assoc 'kind entry)) "Type")
-           (format " [%s]" (cdr (assoc 'desc entry))))
-          (t
-           (replace-regexp-in-string "^[^:]+:[ \n]+" "" (cdr (assoc 'desc entry)))))))
+  (let* ((kind (cdr (assoc 'kind entry)))
+         (desc (cdr (assoc 'desc entry)))
+         (type (cond ((member kind '("Module" "module")) " <module>")
+                     ((string-equal kind "Type") (format " [%s]" desc))
+                     (t desc))))
     (replace-regexp-in-string "[\n ]+" " " type)))
 
 (defun merlin-completion-prefix (ident)

commit 3459e66c81725586fed1102bbfee1a14c6869275

completion: we are printing type schemes, not type exprs.
Fix regression introduced by a6436dc8

src/analysis/browse_misc.ml
@@ -34,12 +34,12 @@ let print_constructor ppf c =
   let open Types in
   match c.cstr_args with
   | [] ->
-    Printtyp.type_expr ppf ({ level = 0 ; id = 0 ; desc = c.cstr_res.desc })
+    Printtyp.type_scheme ppf ({ level = 0 ; id = 0 ; desc = c.cstr_res.desc })
   | args ->
     let desc = Tarrow (Raw_compat.Parsetree.arg_label_of_str "",
                        { level = 0; id = 0; desc = Ttuple args}, c.cstr_res,Cok)
     in
-    Printtyp.type_expr ppf ({ level = 0 ; id = 0 ; desc  })
+    Printtyp.type_scheme ppf ({ level = 0 ; id = 0 ; desc  })
 
 let summary_at pos sum =
   let cmp = Parsing_aux.compare_pos pos in

src/analysis/completion.ml
@@ -159,7 +159,7 @@ let completion_format ~exact name ?path ty =
   let kind =
     match ty with
     | `Value v ->
-      Printtyp.type_expr ppf v.Types.val_type;
+      Printtyp.type_scheme ppf v.Types.val_type;
       `Value
     | `Cons c  ->
       Browse_misc.print_constructor ppf c;