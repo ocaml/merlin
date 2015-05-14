da3411fe0bd3c790e94af825af1f2cf491cf71c8
Frédéric Bour <frederic.bour@lakaban.net>
2015-05-14 09:20:02 -0400

    Summon test suite
    Critical hit, fire damage +15

--- test.sh
+++ test.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+if [ "$1" = "--update" ]; then
+  UPDATE=1
+  shift 1
+else
+  UPDATE=0
+fi 
+
 if test -n "$1" ; then
     out=`mktemp`
     while test -n "$1"; do
-      ./ocamlmerlin.native < ./tests/$1.in  > $out
+      (cd tests && bash $1.in) | ./ocamlmerlin > $out
       # Use more appropriate jsondiff if available
       if [ -n "$DIFF" ]; then
         :
@@ -17,18 +24,24 @@ if test -n "$1" ; then
     done
     rm $out
 else
-    for file in tests/*.in ; do
+    for file in tests/*.in; do
         test_nb=`basename $file .in`
         temp_out=`mktemp`
         real_out=`echo "$file"|sed 's/\.in$/.out/'`
-        ./ocamlmerlin.native < $file  > $temp_out
-        diff $temp_out $real_out > /dev/null
-        if [[ $? != 0 ]] ; then
-            echo -e "$test_nb: \e[1;31mFAILED\e[0m"
-            echo "    run ./test.sh $test_nb to have more informations"
+        (cd tests && bash $test_nb.in) | ./ocamlmerlin > $temp_out
+        if [ "$UPDATE" = 1 ]; then
+            echo "############## $test_nb ##############"
+            diff $temp_out $real_out
+            mv $temp_out $real_out
         else
-            echo -e "$test_nb: \e[1;32mOK\e[0m"
+            diff $temp_out $real_out > /dev/null
+            if [[ $? != 0 ]] ; then
+                echo -e "$test_nb: \e[1;31mFAILED\e[0m"
+                echo "    run ./test.sh $test_nb to have more informations"
+            else
+                echo -e "$test_nb: \e[1;32mOK\e[0m"
+            fi
+            rm $temp_out
         fi
-        rm $temp_out
     done
 fi
