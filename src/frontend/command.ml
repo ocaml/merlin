30f4ce60ff451aca0efdc8a17b3019d20ce7aa84
Thomas Refis <thomas.refis@gmail.com>
2015-06-24 16:35:17 +0100

    error_report: less agressive filtering on ghost locs.
    Follow up of eaaae11 , we only remove warnings now.
    Fixes #410

--- src/frontend/command.ml
+++ src/frontend/command.ml
@@ -590,7 +590,9 @@ let dispatch (state : state) =
         let cmp (l1,_) (l2,_) =
           Lexing.compare_pos l1.Location.loc_start l2.Location.loc_start in
         let err exns =
-          List.filter ~f:(fun (l,_) -> not l.Location.loc_ghost) @@
+          List.filter ~f:(fun (l,err) ->
+            not l.Location.loc_ghost || err.Error_report.where <> "warning"
+          ) @@
           List.sort_uniq ~cmp (List.map ~f:Error_report.of_exn exns)
         in
         let err_lexer  = err (Buffer.lexer_errors state.buffer) in
