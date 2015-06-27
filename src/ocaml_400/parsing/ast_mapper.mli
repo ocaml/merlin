4843e3ccdbf8f8f28cee72afb590e5ea4d2769f2
Frédéric Bour <frederic.bour@lakaban.net>
2015-06-27 21:44:35 +0100

    Drop support for 4.00 and 4.01

--- src/ocaml_400/parsing/ast_mapper.mli
+++
@@ -1,6 +0,0 @@
-(** merlin: manage all internal state *)
-
-type cache
-
-val new_cache : unit -> cache
-val cache : cache ref
--- src/ocaml_401/utils/pparse.ml
+++
@@ -1,2 +0,0 @@
-let apply_rewriters_str ?restore ~tool_name x = x
-let apply_rewriters_sig ?restore ~tool_name x = x
