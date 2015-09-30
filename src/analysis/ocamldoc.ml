7b906e0ae190a7c12b0a46f7793e1e94feb2278a
Frederic Bour <frederic.bour@lakaban.net>
2015-09-30 17:44:30 +0200

    Tweak OCamldoc heuristics for 4.02.3

--- src/analysis/ocamldoc.ml
+++ src/analysis/ocamldoc.ml
@@ -29,6 +29,14 @@ let associate_comment ?(after_only=false) comments loc nextloc =
         let cstart = cloc.Location.loc_start.Lexing.pos_lnum
         and cend =  cloc.Location.loc_end.Lexing.pos_lnum
         in
+        let processed =
+          (* It seems 4.02.3 remove ** from doc comment string, but not from
+           * locations.  We can recognize doc comment by checking how the two
+           * differ. *)
+          (cloc.Location.loc_end.Lexing.pos_cnum -
+           cloc.Location.loc_start.Lexing.pos_cnum) =
+             String.length comment + 5
+        in
         if cend < lstart - 1 || cstart < lend && after_only then
           aux comments
         else if cstart > lend + 1 ||
@@ -37,13 +45,15 @@ let associate_comment ?(after_only=false) comments loc nextloc =
         then
           None, (comment, cloc)::comments
         else if String.length comment < 2 ||
-                comment.[0] <> '*' || comment.[1] = '*'
+                (not processed && (comment.[0] <> '*' || comment.[1] = '*'))
         then
           aux comments
         else
         let comment =
-          String.trim (String.sub comment 1 (String.length comment - 1))
+          if processed then comment else
+          String.sub comment 1 (String.length comment - 1)
         in
+        let comment = String.trim comment in
         match aux comments with
         | None, comments -> Some comment, comments
         | Some c, comments -> Some (String.concat "\n" [comment; c]), comments
