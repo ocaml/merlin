c2197134eb0fb26ae6332633a9fafe4338c38957
Frederic Bour <frederic.bour@lakaban.net>
2016-06-01 15:04:49 +0200

    merlin.install: use opam share_root

--- merlin.install.sh
+++ merlin.install.sh
@@ -8,9 +8,11 @@
   find vim/merlin -type f -a \
     \! \( -name '.*' -o -name '*.pyc' -o -name 'README.md' \) |
   sed -e 's%^[^/]*/[^/]*/\(.*\)%  "&" { "vim/\1" }%' | sort -u
+  printf ']\n'
+  printf 'share_root: [\n'
   for i in $(cd emacs/ && echo *.el); do
-    printf '  "emacs/%s" {"../emacs/site-lisp/%s"}\n' "$i" "$i"
-    printf '  "?emacs/%sc" {"../emacs/site-lisp/%sc"}\n' "$i" "$i"
+    printf '  "emacs/%s" {"emacs/site-lisp/%s"}\n' "$i" "$i"
+    printf '  "?emacs/%sc" {"emacs/site-lisp/%sc"}\n' "$i" "$i"
   done
   printf ']\n'
 ) > merlin.install
