5a991b295770e750f72008551c1360514c668224
Frederic Bour <frederic.bour@lakaban.net>
2016-08-29 15:15:51 +0100

    prepare for future backward compatibility in protocol selection

--- src/frontend/ocamlmerlin.ml
+++ src/frontend/ocamlmerlin.ml
@@ -1,6 +1,7 @@
 let main () =
   match List.tl (Array.to_list Sys.argv) with
   | "merlin3" :: args -> New_merlin.run args
+  | "merlin-old" :: args -> Old_merlin.run args
   | args -> Old_merlin.run args
 
 let () = main ()
