57994f47988591c047330e783ac729595c0f6464
Frédéric Bour <frederic.bour@lakaban.net>
2017-01-31 17:50:30 +0000

    add caching of mtyper

--- src/kernel/mtyper.ml
+++ src/kernel/mtyper.ml
@@ -1,23 +1,127 @@
 open Std
 
+type ('p,'t) item = {
+  parsetree_item: 'p;
+  typedtree_items: 't list * Types.signature_item list;
+  part_snapshot: Btype.snapshot;
+  part_env : Env.t;
+  part_errors    : exn list;
+  part_checks    : Typecore.delayed_check list;
+}
+
 type typedtree = [
   | `Interface of Typedtree.signature
   | `Implementation of Typedtree.structure
 ]
 
 type result = {
-  id        : int;
   config    : Mconfig.t;
   state     : Mocaml.typer_state;
-  errors    : exn list;
-  checks    : Typecore.delayed_check list;
-  typedtree : typedtree;
+  initial_env : Env.t;
+  initial_snapshot : Btype.snapshot;
+  typedtree : [
+    | `Interface of
+        (Parsetree.signature_item, Typedtree.signature_item) item list
+    | `Implementation of
+        (Parsetree.structure_item, Typedtree.structure_item) item list
+  ];
 }
 
-let id = let k = ref 0 in fun () -> incr k; !k
+let cache = ref None
 
-let print_result () {id; _} =
-  sprintf "<Mtyper.result%d>" id
+let print_result () _ = "<Mtyper.result>"
+
+let compatible_prefix result_items tree_items =
+  let rec aux acc = function
+    | (ritem :: ritems, pitem :: pitems)
+      when Btype.is_valid ritem.part_snapshot
+        && compare ritem.parsetree_item pitem = 0 ->
+      aux (ritem :: acc) (ritems, pitems)
+    | (_, pitems) -> acc, pitems
+  in
+  aux [] (result_items, tree_items)
+
+let fresh_env config =
+  let env0 = Typer_raw.fresh_env () in
+  let env0 = Env.open_pers_signature "Pervasives" env0 in
+  let env0 = Extension.register Mconfig.(config.merlin.extensions) env0 in
+  let snap0 = Btype.snapshot () in
+  (env0, snap0)
+
+let rec type_structure caught env = function
+  | parsetree_item :: rest ->
+    let items, _, part_env =
+      Typemod.type_structure env [parsetree_item]
+        parsetree_item.Parsetree.pstr_loc in
+    let typedtree_items =
+      (items.Typedtree.str_items, items.Typedtree.str_type) in
+    let item = {
+      parsetree_item; typedtree_items;
+      part_snapshot = Btype.snapshot ();
+      part_errors = !caught;
+      part_checks = !Typecore.delayed_checks;
+      part_env
+    } in
+    item :: type_structure caught part_env rest
+  | [] -> []
+
+let rec type_signature caught env = function
+  | parsetree_item :: rest ->
+    let {Typedtree. sig_final_env = part_env; sig_items; sig_type} =
+      Typemod.transl_signature env [parsetree_item] in
+    let item = {
+      parsetree_item; typedtree_items = (sig_items, sig_type);
+      part_snapshot = Btype.snapshot ();
+      part_errors = !caught;
+      part_checks = !Typecore.delayed_checks;
+      part_env
+    } in
+    item :: type_signature caught part_env rest
+  | [] -> []
+
+let type_implementation config caught cached parsetree =
+  let env0, snap0, (prefix, parsetree)  =
+    match cached with
+    | Some (env0, snap0, `Implementation items) when Btype.is_valid snap0 ->
+      env0, snap0, compatible_prefix items parsetree
+    | Some (env0, snap0, `Interface _)  when Btype.is_valid snap0 ->
+      env0, snap0, ([], parsetree)
+    | Some _ | None ->
+      let env0, snap0 = fresh_env config in
+      env0, snap0, ([], parsetree)
+  in
+  let env', snap' = match prefix with
+    | [] -> (env0, snap0)
+    | x :: _ ->
+      caught := x.part_errors;
+      Typecore.delayed_checks := x.part_checks;
+      (x.part_env, x.part_snapshot)
+  in
+  Btype.backtrack snap';
+  let suffix = type_structure caught env' parsetree in
+  env0, snap0, List.rev_append prefix suffix
+
+let type_interface config caught cached parsetree =
+  let env0, snap0, (prefix, parsetree)  =
+    match cached with
+    | Some (env0, snap0, `Interface items) when Btype.is_valid snap0 ->
+      env0, snap0, compatible_prefix items parsetree
+    | Some (env0, snap0, `Implementation _)  when Btype.is_valid snap0 ->
+      env0, snap0, ([], parsetree)
+    | Some _ | None ->
+      let env0, snap0 = fresh_env config in
+      env0, snap0, ([], parsetree)
+  in
+  let env', snap' = match prefix with
+    | [] -> (env0, snap0)
+    | x :: _ ->
+      caught := x.part_errors;
+      Typecore.delayed_checks := x.part_checks;
+      (x.part_env, x.part_snapshot)
+  in
+  Btype.backtrack snap';
+  let suffix = type_signature caught env' parsetree in
+  env0, snap0, List.rev_append prefix suffix
 
 let run tr config source parsetree =
   Trace.enter tr
@@ -30,54 +134,72 @@ let run tr config source parsetree =
     ~return:print_result
   @@ fun tr ->
   Mocaml.setup_config config;
-  let state = Mocaml.new_state ~unit_name:(Msource.unitname source) in
+  let cached, state = match !cache with
+    | Some (config',state,cached) when compare config' config = 0 ->
+      (Some cached, state)
+    | Some _ | None ->
+      cache := None;
+      (None, Mocaml.new_state ~unit_name:(Msource.unitname source))
+  in
   Mocaml.with_state state @@ fun () ->
   let caught = ref [] in
   Msupport.catch_errors Mconfig.(config.ocaml.warnings) caught @@ fun () ->
   Typecore.reset_delayed_checks ();
-  let env0 = Typer_raw.fresh_env () in
-  let env0 = Env.open_pers_signature "Pervasives" env0 in
-  let env0 = Extension.register Mconfig.(config.merlin.extensions) env0 in
-  let location = {
-    Location.
-    loc_start = Msource.get_lexing_pos tr source `Start;
-    loc_end = Msource.get_lexing_pos tr source `End;
-    loc_ghost = false;
-  } in
-  let typedtree = match parsetree with
-    | `Implementation impl ->
-      let tree, sg, env = Typemod.type_structure env0 impl location in
-      `Implementation tree
-    | `Interface intf ->
-      let sg = Typemod.transl_signature env0 intf in
-      `Interface sg
+  let result = match parsetree with
+    | `Implementation parsetree ->
+      let initial_env, initial_snapshot, items =
+        type_implementation config caught cached parsetree in
+      { config; state; initial_env; initial_snapshot;
+        typedtree = `Implementation items }
+    | `Interface parsetree ->
+      let initial_env, initial_snapshot, items =
+        type_interface config caught cached parsetree in
+      { config; state; initial_env; initial_snapshot;
+        typedtree = `Interface items }
   in
-  let checks = !Typecore.delayed_checks in
-  let errors = !caught in
   Typecore.reset_delayed_checks ();
-  { id = id (); config; state; typedtree; checks; errors }
+  result
 
 let with_typer t f =
   Mocaml.with_state t.state f
 
-let get_typedtree t =
-  assert (Mocaml.is_state t.state);
-  t.typedtree
-
 let get_env ?pos t =
   assert (Mocaml.is_state t.state);
-  match t.typedtree with
-  | `Implementation str -> str.Typedtree.str_final_env
-  | `Interface sg -> sg.Typedtree.sig_final_env
+  Option.value ~default:t.initial_env (
+    match t.typedtree with
+    | `Implementation l -> Option.map ~f:(fun x -> x.part_env) (List.last l)
+    | `Interface l ->  Option.map ~f:(fun x -> x.part_env) (List.last l)
+  )
 
 let get_errors t =
   assert (Mocaml.is_state t.state);
-  let caught = ref t.errors in
-  Typecore.delayed_checks := t.checks;
+  let errors, checks = Option.value ~default:([],[]) (
+      let f x = x.part_errors, x.part_checks in
+      match t.typedtree with
+      | `Implementation l -> Option.map ~f (List.last l)
+      | `Interface l ->  Option.map ~f (List.last l)
+    )
+  in
+  let caught = ref errors in
+  Typecore.delayed_checks := checks;
   Msupport.catch_errors Mconfig.(t.config.ocaml.warnings) caught
     Typecore.force_delayed_checks;
   Typecore.reset_delayed_checks ();
   (!caught)
+
+let get_typedtree t =
+  assert (Mocaml.is_state t.state);
+  let split_items l =
+    let typd, typs = List.split (List.map ~f:(fun x -> x.typedtree_items) l) in
+    (List.concat typd, List.concat typs)
+  in
+  match t.typedtree with
+  | `Implementation l ->
+    let str_items, str_type = split_items l in
+    `Implementation {Typedtree. str_items; str_type; str_final_env = get_env t}
+  | `Interface l ->
+    let sig_items, sig_type = split_items l in
+    `Interface {Typedtree. sig_items; sig_type; sig_final_env = get_env t}
 
 let node_at tr ?(skip_recovered=false) t pos_cursor =
   Trace.enter tr "Mtyper.node_at %a %a"
