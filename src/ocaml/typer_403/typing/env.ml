2d4c1630a44326f6498b8621176f536a14a14033
Frédéric Bour <frederic.bour@lakaban.net>
2017-02-06 17:01:36 +0000

    Env 4.03: measure time spent in find_in_path_uncap

--- src/ocaml/typer_403/typing/env.ml
+++ src/ocaml/typer_403/typing/env.ml
@@ -2145,24 +2145,32 @@ let () =
     )
 
 let check_state_consistency () =
-  try
-    Hashtbl.iter (fun name ps ->
-        let filename =
-          try Some (find_in_path_uncap !load_path (name ^ ".cmi"))
-          with Not_found -> None
-        in
-        let invalid =
-          match filename, ps with
-          | None, None -> false
-          | Some filename, Some ps ->
-            begin match !(Cmi_cache.(read filename).Cmi_cache.cmi_cache) with
-              | Cmi_cache_store (_, _, ps_sig) ->
-                not (Std.lazy_eq ps_sig ps.ps_sig)
-              | _ -> true
-            end
-          | _, _ -> true
-        in
-        if invalid then raise Not_found
-      ) !persistent_structures;
-    true
-  with Not_found -> false
+  let total = ref 0.0 in
+  let result =
+    try
+      Hashtbl.iter (fun name ps ->
+          let time = Sys.time () in
+          let filename =
+            try Some (find_in_path_uncap !load_path (name ^ ".cmi"))
+            with Not_found -> None
+          in
+          total := !total +. (Sys.time () -. time);
+          let invalid =
+            match filename, ps with
+            | None, None -> false
+            | Some filename, Some ps ->
+              begin match !(Cmi_cache.(read filename).Cmi_cache.cmi_cache) with
+                | Cmi_cache_store (_, _, ps_sig) ->
+                  not (Std.lazy_eq ps_sig ps.ps_sig)
+                | _ -> true
+              end
+            | _, _ -> true
+          in
+          if invalid then raise Not_found
+        ) !persistent_structures;
+      true
+    with Not_found -> false
+  in
+  Logger.logf "Env" "check_state_consistency"
+    "spent %4.02fms in find_in_path_uncap"  !total;
+  result
