455d45774d024f601b9ce6ee6e87ba0e1ae1fce0
Frédéric Bour <frederic.bour@lakaban.net>
2017-02-07 13:20:23 +0000

    Mppx: change directory

--- src/kernel/mppx.ml
+++ src/kernel/mppx.ml
@@ -1,9 +1,31 @@
 open Mconfig
 
+let change_directory dir =
+  Logger.log "Mppx" "changing_directory" dir;
+  match Sys.chdir dir with
+  | () -> true
+  | exception exn ->
+    Logger.logf "Mppx" "changing directory"
+      "chang_directory %S failed with %t" dir
+      (fun () -> Printexc.to_string exn);
+    false
+
+
 let rewrite _trace cfg parsetree =
   let ppx = cfg.ocaml.ppx @ Ppxsetup.command_line cfg.merlin.packages_ppx in
+  let prev_dir = Sys.getcwd () in
+  let restore () =
+    if not (change_directory prev_dir) then
+      ignore (change_directory "/")
+  in
+  ignore (change_directory cfg.query.directory);
   match Pparse.apply_rewriters ~ppx ~tool_name:"merlin" parsetree with
-  | parsetree -> cfg, parsetree
+  | parsetree ->
+    restore ();
+    cfg, parsetree
   | exception exn ->
+    Logger.logf "Mppx" "rewrite"
+      "failed with %t" (fun () -> Printexc.to_string exn);
     Msupport.raise_error exn;
+    restore ();
     cfg, parsetree
