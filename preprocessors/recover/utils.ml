de93db2114e9161cdfebf340458826237c5d8652
Frédéric Bour <frederic.bour@lakaban.net>
2017-02-14 13:47:58 +0100

    preprocess recover: filter duplicate cases

--- preprocessors/recover/utils.ml
+++ preprocessors/recover/utils.ml
@@ -7,16 +7,19 @@ let const c = fun _ -> c
 
 let group_assoc l =
   let cons k v acc = (k, List.rev v) :: acc in
-  let rec aux k v acc = function
-    | [] -> List.rev (cons k v acc)
+  let rec aux k v vs acc = function
+    | [] -> List.rev (cons k (v :: vs) acc)
     | (k', v') :: xs when compare k k' = 0 ->
-        aux k (v' :: v) acc xs
+      if compare v v' = 0 then
+        aux k v vs acc xs
+      else
+        aux k v' (v :: vs) acc xs
     | (k', v') :: xs ->
-        aux k' [v'] (cons k v acc) xs
+      aux k' v' [] (cons k (v :: vs) acc) xs
   in
   match List.sort compare l with
   | [] -> []
-  | (k, v) :: xs -> aux k [v] [] xs
+  | (k, v) :: xs -> aux k v [] [] xs
 
 (* negation to put nan as the max *)
 let compare_float a b = - compare (-.a) (-.b)
