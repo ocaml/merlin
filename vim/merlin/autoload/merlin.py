e292587806484d9475ce834232cff0e6add276c1
Thomas Refis <thomas.refis@gmail.com>
2017-03-07 11:31:39 +0000

    vim: pass -dot-merlin to merlin when necessary

--- vim/merlin/autoload/merlin.py
+++ vim/merlin/autoload/merlin.py
@@ -155,6 +155,7 @@ def command2(args, context=None, track_verbosity=None):
     cmdline = ["server"] + list(args) + ["-filename",filename] + verbosity + \
             concat_map(lambda ext: ("-extension",ext), vim_list_if_set("b:merlin_extensions")) + \
             concat_map(lambda pkg: ("-package",pkg), vim_list_if_set("b:merlin_packages")) + \
+            concat_map(lambda dm: ("-dot-merlin",dm), vim_list_if_set("b:merlin_dot_merlins")) + \
             vim.eval('g:merlin_binary_flags') + \
             vim_list_if_set('b:merlin_flags')
 
