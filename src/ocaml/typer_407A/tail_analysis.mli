4ce44149c43b06a9227b67cac93a51eb9f94dcbd
Thomas Refis <thomas.refis@gmail.com>
2018-02-22 15:52:29 +0000

    duplicate typer_407 to typer_407A
    To test various GPRs modifying types.ml[i]

---
+++ src/ocaml/typer_407A/tail_analysis.mli
@@ -0,0 +1,40 @@
+(* {{{ COPYING *(
+
+  This file is part of Merlin, an helper for ocaml editors
+
+  Copyright (C) 2013 - 2015  Frédéric Bour  <frederic.bour(_)lakaban.net>
+                             Thomas Refis  <refis.thomas(_)gmail.com>
+                             Simon Castellan  <simon.castellan(_)iuwt.fr>
+
+  Permission is hereby granted, free of charge, to any person obtaining a
+  copy of this software and associated documentation files (the "Software"),
+  to deal in the Software without restriction, including without limitation the
+  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+  sell copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  The Software is provided "as is", without warranty of any kind, express or
+  implied, including but not limited to the warranties of merchantability,
+  fitness for a particular purpose and noninfringement. In no event shall
+  the authors or copyright holders be liable for any claim, damages or other
+  liability, whether in an action of contract, tort or otherwise, arising
+  from, out of or in connection with the software or the use or other dealings
+  in the Software.
+
+)* }}} *)
+
+open Parsetree
+
+(* Assuming an expression (or other constructs occuring inside expressions,
+   like cases of a match statement) is in tail-position, returns all
+   sub-expression that will be evaluated in tail-position too *)
+val tail_positions: Browse_raw.node -> Browse_raw.node list
+
+(* If the node is a function, return all of its entry-points -- those are in
+   tail-position. Returns an empty list otherwise *)
+val entry_points: Browse_raw.node -> Browse_raw.node list
+
+val is_call: Browse_raw.node -> bool
---
+++ src/ocaml/typer_407A/utils/config.ml
@@ -0,0 +1,65 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           *)
+(*                                                                        *)
+(*   Copyright 1996 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(***********************************************************************)
+(**                                                                   **)
+(**               WARNING WARNING WARNING                             **)
+(**                                                                   **)
+(** When you change this file, you must make the parallel change      **)
+(** in config.mlbuild                                                 **)
+(**                                                                   **)
+(***********************************************************************)
+
+
+(* The main OCaml version string has moved to ../VERSION *)
+let version = Sys.ocaml_version
+
+let windows =
+  match Sys.os_type with
+  | "Win32" -> true
+  |    _    -> false
+
+let sf = Printf.sprintf
+
+let flambda = false
+
+let exec_magic_number = "Caml1999X011"
+and cmi_magic_number = "Caml1999I022"
+and cmo_magic_number = "Caml1999O022"
+and cma_magic_number = "Caml1999A022"
+and cmx_magic_number =
+  if flambda then
+    "Caml1999y022"
+  else
+    "Caml1999Y022"
+and cmxa_magic_number =
+  if flambda then
+    "Caml1999z022"
+  else
+    "Caml1999Z022"
+and ast_impl_magic_number = "Caml1999M022"
+and ast_intf_magic_number = "Caml1999N022"
+and cmxs_magic_number = "Caml1999D022"
+    (* cmxs_magic_number is duplicated in otherlibs/dynlink/natdynlink.ml *)
+and cmt_magic_number = "Caml1999T022"
+
+let load_path = ref ([] : string list)
+
+let interface_suffix = ref ".mli"
+
+let max_tag = 245
+
+let safe_string = false
+let flat_float_array = false
