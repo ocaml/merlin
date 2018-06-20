0ec353f58ab204e40c7ab0b72a7461885bb99eac
nojebar <nicolas.ojeda.bar@lexifi.com>
2018-06-20 16:51:08 +0200

    Fix gen_my_config.ml

--- src/config/gen_my_config.ml
+++ src/config/gen_my_config.ml
@@ -3,14 +3,10 @@ let ocaml_version_val s =
     Scanf.sscanf s "%[0-9].%[0-9].%[0-9]" (fun maj min patch -> maj, min, patch)
   in
   match maj, min, patch with
-  | "4", "02", "0" ->
-     "`OCaml_4_02_0"
-  | "4", "02", "1" ->
-     "`OCaml_4_02_1"
-  | "4", "02", "2" ->
-     "`OCaml_4_02_2"
   | "4", "02", "3" ->
      "`OCaml_4_02_3"
+  | "4", "03", _ ->
+     "`OCaml_4_03_0"
   | "4", "04", _ ->
      "`OCaml_4_04_0"
   | "4", "05", _ ->
