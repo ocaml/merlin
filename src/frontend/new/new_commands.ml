08eb401b06a0a724c9eec6b7ce82aad1597a570f
Frédéric Bour <def@fb.com>
2018-07-09 13:41:25 +0100

    implement dump command for server

--- src/frontend/new/new_commands.ml
+++ src/frontend/new/new_commands.ml
@@ -567,6 +567,20 @@ The return value has the shape:
   ;
 
   (* Used only for testing *)
+  command "dump"
+    ~spec:[
+      arg "-what" "<parsetree|printast|env|fullenv|browse|tokens|flags\
+                   |warnings|exn|paths> Information to dump ()"
+        (Marg.param "string" (fun what _ -> what));
+    ]
+    ~default:""
+    ~doc:"Not for the casual user, used for debugging merlin"
+    begin fun pipeline what ->
+      run pipeline (Query_protocol.Dump [`String what])
+    end
+  ;
+
+  (* Used only for testing *)
   command "dump-configuration" ~spec:[] ~default:()
     ~doc:"Not for the casual user, used for merlin tests"
     begin fun pipeline () ->
