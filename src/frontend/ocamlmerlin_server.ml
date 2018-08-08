fa2cb1c39fc05e8e89b1ce2d42991a42b03a093a
Ewen Maclean <ewenmaclean@gmail.com>
2018-08-08 16:28:02 +0100

    updating single command passing entire environment as argument

--- src/frontend/ocamlmerlin_server.ml
+++ src/frontend/ocamlmerlin_server.ml
@@ -73,18 +73,20 @@ let main () =
   (* Setup env for extensions *)
   Unix.putenv "__MERLIN_MASTER_PID" (string_of_int (Unix.getpid ()));
   match List.tl (Array.to_list Sys.argv) with
-  | "single" :: args -> exit (New_merlin.run ("PATH=" ^ Unix.getenv "PATH" ^ "\000") None args)
+  | "single" :: args -> exit (New_merlin.run 
+    (String.concat "" (Array.to_list (Array.map (fun s -> s ^ "\000") (Unix.environment ())))) 
+     args)
   | "old-protocol" :: args -> Old_merlin.run args
   | ["server"; socket_path; socket_fd] -> Server.start socket_path socket_fd
   | ("-help" | "--help" | "-h" | "server") :: _ ->
     Printf.eprintf
       "Usage: %s <frontend> <arguments...>\n\
        Select the merlin frontend to execute. Valid values are:\n\
-      \n- 'old-protocol' executes the merlin frontend from previous version.\n\
+       \n- 'old-protocol' executes the merlin frontend from previous version.\n\
       \  It is a top level reading and writing commands in a JSON form.\n\
-      \n- 'single' is a simpler frontend that reads input from stdin,\n\
+       \n- 'single' is a simpler frontend that reads input from stdin,\n\
       \  processes a single query and outputs result on stdout.\n\
-      \n- 'server' works like 'single', but uses a background process to\n\
+       \n- 'server' works like 'single', but uses a background process to\n\
       \  speedup processing.\n\
        If no frontend is specified, it defaults to 'old-protocol' for\n\
        compatibility with existing editors.\n"
