49269af6c23a93620b609ba1f05f40507c855425
Andrey Popp <8mayday@gmail.com>
2019-02-04 21:55:12 +0300

    ocamlmerlin-lsp: split completion tests

---
+++ tests-lsp/__tests__/textDocument-completion.test.ts
@@ -0,0 +1,105 @@
+import * as LanguageServer from "./../src/LanguageServer";
+
+import * as Protocol from "vscode-languageserver-protocol";
+import * as Types from "vscode-languageserver-types";
+
+describe("textDocument/completion", () => {
+  it("completes identifier at top level", async () => {
+    let languageServer = await LanguageServer.startAndInitialize();
+    let source = `
+Strin
+      `.trim();
+    await languageServer.sendNotification("textDocument/didOpen", {
+      textDocument: Types.TextDocumentItem.create(
+        "file:///test.ml",
+        "txt",
+        0,
+        source
+      )
+    });
+
+    let result = await languageServer.sendRequest("textDocument/completion", {
+      textDocument: Types.TextDocumentIdentifier.create("file:///test.ml"),
+      position: Types.Position.create(0, 4)
+    });
+    expect(result).toMatchObject({
+      isIncomplete: false,
+      items: [
+        { label: "StringLabels", detail: "" },
+        { label: "String", detail: "" }
+      ]
+    });
+    await LanguageServer.exit(languageServer);
+  });
+
+  it("completes identifier at top level", async () => {
+    let languageServer = await LanguageServer.startAndInitialize();
+    let source = `
+String.
+      `.trim();
+    await languageServer.sendNotification("textDocument/didOpen", {
+      textDocument: Types.TextDocumentItem.create(
+        "file:///test.ml",
+        "txt",
+        0,
+        source
+      )
+    });
+
+    let result: any = await languageServer.sendRequest(
+      "textDocument/completion",
+      {
+        textDocument: Types.TextDocumentIdentifier.create("file:///test.ml"),
+        position: Types.Position.create(0, 7)
+      }
+    );
+    let items = result.items.map(item => item.label);
+    expect(items).toMatchObject([
+      "blit",
+      "capitalize",
+      "capitalize_ascii",
+      "compare",
+      "concat",
+      "contains",
+      "contains_from",
+      "copy",
+      "create",
+      "equal",
+      "escaped",
+      "fill",
+      "get",
+      "index",
+      "index_from",
+      "index_from_opt",
+      "index_opt",
+      "init",
+      "iter",
+      "iteri",
+      "length",
+      "lowercase",
+      "lowercase_ascii",
+      "make",
+      "map",
+      "mapi",
+      "rcontains_from",
+      "rindex",
+      "rindex_from",
+      "rindex_from_opt",
+      "rindex_opt",
+      "set",
+      "split_on_char",
+      "sub",
+      "trim",
+      "uncapitalize",
+      "uncapitalize_ascii",
+      "unsafe_blit",
+      "unsafe_fill",
+      "unsafe_get",
+      "unsafe_set",
+      "uppercase",
+      "uppercase_ascii",
+      "t"
+    ]);
+    await LanguageServer.exit(languageServer);
+  });
+});
