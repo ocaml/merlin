92f665548b35bb1a163e8e2d3f1952542c0503f2
Andrey Popp <8mayday@gmail.com>
2019-03-08 20:04:21 +0300

    LSP: format docstrings

--- src/lsp/protocol.ml
+++ src/lsp/protocol.ml
@@ -224,16 +224,19 @@ module WorkspaceEdit = struct
   type documentChanges = TextDocumentEdit.t list [@@deriving to_yojson]
 
   (**
-     Depending on the client capability [workspace.workspaceEdit.resourceOperations] document changes
-     are either an array of [TextDocumentEdit]s to express changes to n different text documents
-     where each text document edit addresses a specific version of a text document. Or it can contain
-     above [TextDocumentEdit]s mixed with create, rename and delete file / folder operations.
+     Depending on the client capability
+     [workspace.workspaceEdit.resourceOperations] document changes are either an
+     array of [TextDocumentEdit]s to express changes to n different text
+     documents where each text document edit addresses a specific version of a
+     text document. Or it can contain above [TextDocumentEdit]s mixed with
+     create, rename and delete file / folder operations.
 
      Whether a client supports versioned document edits is expressed via
      [workspace.workspaceEdit.documentChanges] client capability.
 
-     If a client neither supports [documentChanges] nor [workspace.workspaceEdit.resourceOperations] then
-     only plain [TextEdit]s using the [changes] property are supported.
+     If a client neither supports [documentChanges] nor
+     [workspace.workspaceEdit.resourceOperations] then only plain [TextEdit]s
+     using the [changes] property are supported.
   *)
   type t = {
     changes: changes option;
