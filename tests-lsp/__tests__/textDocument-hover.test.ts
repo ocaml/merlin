58a0e91a83558cf36632518f40d85623133fbaec
Louis Roché <louis@louisroche.net>
2019-03-23 03:53:11 +0000

    LSP: add test to show bug in position translation

--- tests-lsp/__tests__/textDocument-hover.test.ts
+++ tests-lsp/__tests__/textDocument-hover.test.ts
@@ -1,3 +1,4 @@
+import outdent from "outdent";
 import * as LanguageServer from "./../src/LanguageServer";
 
 import * as Protocol from "vscode-languageserver-protocol";
@@ -88,6 +89,48 @@ describe("textDocument/hover", () => {
       contents: {
         kind: "markdown",
         value: "```ocaml\n'a -> 'a\n(** This function has a nice documentation *)\n```" }
+    });
+  });
+
+  // This type highlights an issue with conversion of positions
+  // from LSP to merlin. The position is shifted to the right by 1.
+  it("returns wrong type when cursor is between values", async () => {
+    languageServer = await LanguageServer.startAndInitialize({
+      textDocument: {
+        hover: {
+          dynamicRegistration: true,
+          contentFormat: ["markdown", "plaintext"]
+        }
+      }
+    });
+    await languageServer.sendNotification("textDocument/didOpen", {
+      textDocument: Types.TextDocumentItem.create(
+        "file:///test.ml",
+        "txt",
+        0,
+        outdent`
+          let f i f = float_of_int i +. f
+          let i = 10
+          let f = 10.
+          let sum = f i f
+       `
+      )
+    });
+
+    let result = await languageServer.sendRequest("textDocument/hover", {
+      textDocument: Types.TextDocumentIdentifier.create("file:///test.ml"),
+      position: Types.Position.create(3, 13)
+    });
+
+    expect(result).toMatchObject({
+      contents: {
+        kind: "markdown",
+        value: "```ocaml\nfloat\n```"
+      },
+      range: {
+        start: {"character": 14, "line": 3},
+        end: {"character": 15, "line": 3},
+      }
     });
   });
 });
