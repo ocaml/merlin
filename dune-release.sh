dac3ca6d24a3c8d62ac522fcd44a53487b70f415
Frédéric Bour <def@fb.com>
2019-07-15 14:52:36 +0200

    Please the gods of release

--- dune-release.sh
+++ dune-release.sh
@@ -3,7 +3,7 @@
 TAG="$1"
 
 if [ -z "$TAG" ]; then
-  printf "Usage: ./release.sh <tag-name>\n"
+  printf "Usage: ./dune-release.sh <tag-name>\n"
   printf "Please make sure that dune-release is available.\n"
   exit 1
 fi
@@ -16,10 +16,10 @@ step()
   if [ "x$action" == "xN" ]; then exit 2; fi
 }
 
-dune-release distrib -p merlin -n merlin -t "$TAG" --skip-tests --skip-lint
+dune-release distrib -p merlin -n merlin -t "$TAG" --skip-tests #--skip-lint
 step
-dune-release publish distrib --change-log=CHANGELOG.md -p merlin -n merlin -t "$TAG"
+dune-release publish distrib -p merlin -n merlin -t "$TAG"
 step
-dune-release opam pkg --change-log=CHANGELOG.md -p merlin -n merlin -t "$TAG"
+dune-release opam pkg -p merlin -n merlin -t "$TAG"
 step
-dune-release opam submit --change-log=CHANGELOG.md -p merlin -n merlin -t "$TAG"
+dune-release opam submit -p merlin -n merlin -t "$TAG"
