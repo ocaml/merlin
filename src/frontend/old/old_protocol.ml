31ad94648222b90ebff019fd44ad47f249991d41
Gabor Greif <ggreif@gmail.com>
2019-09-21 12:23:10 +0200

    remove deprecation warnings

--- src/frontend/old/old_protocol.ml
+++ src/frontend/old/old_protocol.ml
@@ -94,5 +94,5 @@ type request = Request : Context.t * 'a command -> request
 type response =
   | Return    : 'a command * 'a -> response
   | Failure   : string -> response
-  | Error     : json -> response
+  | Error     : Json.t -> response
   | Exception : exn -> response
