9ffdfcc2c0fbe2e999ea7a32a4a13df9cf890eed
Gabor Greif <ggreif@gmail.com>
2019-10-06 16:33:05 +0200

    Remove more deprecation warnings

--- src/lsp/uri.mli
+++ src/lsp/uri.mli
@@ -1,7 +1,7 @@
 type t
 
-val of_yojson : Yojson.Safe.json -> (t, string) result
-val to_yojson : t -> Yojson.Safe.json
+val of_yojson : Yojson.Safe.t -> (t, string) result
+val to_yojson : t -> Yojson.Safe.t
 
 val to_path : t -> string
 val of_path : string -> t
