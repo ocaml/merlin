fdd0aff17aa6e54140f538f1b67e951582a9acf1
Frédéric Bour <fred@tarides.com>
2019-11-22 17:41:17 +0900

    Local_store: fix is_bound implementation

--- src/utils/local_store.ml
+++ src/utils/local_store.ml
@@ -1,11 +1,14 @@
 open Std
 
 type ref_and_reset = F : 'a ref * (unit -> 'a) -> ref_and_reset
-type bindings = { mutable refs: ref_and_reset list; is_bound: bool ref }
+type bindings = {
+  mutable refs: ref_and_reset list;
+  mutable frozen : bool;
+  is_bound: bool ref
+}
 
 let new_bindings () =
-  let is_bound = ref false in
-  { refs = [F (is_bound, (fun () -> true))]; is_bound }
+  { refs = []; is_bound = ref false; frozen = false }
 
 let is_bound t = !(t.is_bound)
 
@@ -15,29 +18,36 @@ let reset t =
 
 let ref t f =
   let result = ref (f ()) in
+  assert (not t.frozen);
   t.refs <- (F (result, f)) :: t.refs;
   result
 
 type 'a slot = { ref : 'a ref; mutable value : 'a }
 type a_slot = Slot : 'a slot -> a_slot
-type scope = a_slot list
+type scope = { slots: a_slot list; scope_bound : bool ref }
 
 let fresh t =
-  List.map ~f:(fun (F(ref,f)) -> Slot {ref; value = f ()}) t.refs
+  t.frozen <- true;
+  { slots = List.map ~f:(fun (F(ref,f)) -> Slot {ref; value = f ()}) t.refs;
+    scope_bound = t.is_bound }
 
 type ref_and_value = V : 'a ref * 'a -> ref_and_value
 let restore l = List.iter ~f:(fun (V(r,v)) -> r := v) l
 
-let with_scope scope f =
-  let backup = List.rev_map ~f:(fun (Slot {ref;_}) -> V (ref,!ref)) scope in
-  List.iter ~f:(fun (Slot {ref;value}) -> ref := value) scope;
+let with_scope { slots; scope_bound } f =
+  assert (not !scope_bound);
+  scope_bound := true;
+  let backup = List.rev_map ~f:(fun (Slot {ref;_}) -> V (ref,!ref)) slots in
+  List.iter ~f:(fun (Slot {ref;value}) -> ref := value) slots;
   match f () with
   | x ->
-    List.iter ~f:(fun (Slot s) -> s.value <- !(s.ref)) scope;
+    List.iter ~f:(fun (Slot s) -> s.value <- !(s.ref)) slots;
+    scope_bound := false;
     restore backup;
     x
   | exception exn ->
-    List.iter ~f:(fun (Slot s) -> s.value <- !(s.ref)) scope;
+    List.iter ~f:(fun (Slot s) -> s.value <- !(s.ref)) slots;
+    scope_bound := false;
     restore backup;
     reraise exn
 
