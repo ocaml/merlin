0c888a68cee95203b1ba87f70a640a5468f6e6e8
voodoos <thevoodoos@gmail.com>
2020-05-28 12:11:12 +0200

    Remove debug comment from test

--- tests/test-dirs/errors/alerts/main.ml
+++ tests/test-dirs/errors/alerts/main.ml
@@ -1,25 +1,2 @@
 open Lib
 let x = sqrt 3.
-
-(* ocamlmerlin single errors -filename main.ml < main.ml | jq *)
-
-(* C'est rigolo, l'erreur "leak"
-```
-Error (alert deprecated): Lib.sqrt
-I am deprecated
-{
-  "class": "return",
-  "value": [],
-  "notifications": [],
-  "timing": {
-    "clock": 4,
-    "cpu": 0,
-    "query": 0,
-    "pp": 0,
-    "reader": 0,
-    "ppx": 0,
-    "typer": 0,
-    "error": 0
-  }
-}
-``` *)
