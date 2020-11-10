4e1fab3bd34d370f712b99c27930db16f95722f9
Ulysse Gérard <thevoodoos@gmail.com>
2020-11-10 11:33:34 +0100

    403 compat

--- src/ocaml/typing/403/typecore.mli
+++ src/ocaml/typing/403/typecore.mli
@@ -154,3 +154,16 @@ val constant: Parsetree.constant -> (Asttypes.constant, error) result
 
 val extract_concrete_record :
   Env.t -> Types.type_expr -> Path.t * Path.t * Types.label_declaration list
+
+(* Merlin specific *)
+type type_pat_mode
+val partial_pred :
+  lev:int ->
+  ?mode:type_pat_mode ->
+  ?explode:int ->
+  Env.t ->
+  type_expr ->
+  (label, constructor_description) Hashtbl.t ->
+  (label, label_description) Hashtbl.t ->
+  Parsetree.pattern ->
+  Typedtree.pattern option
