e2048f2d2191e7a7e2a8680ea786336608dad6eb
Kate <kit.ty.kate@disroot.org>
2021-07-07 15:18:07 +0100

    Add upstream-upgrader.sh script

---
+++ upstream-upgrader.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -e
+
+if test $# != 1; then
+    echo "Usage: $0 [ocaml version]"
+    echo "  e.g. $0 4.13"
+    exit 1
+fi
+
+CWD=$(dirname "$0")
+
+if test "${CWD}" != '.'; then
+    echo "This script can only be called from the root directory of merlin."
+    exit 1
+fi
+
+case "$1" in
+[0-9]\.[0-9][0-9]) ;;
+*) echo "Could not parse the version number"; exit 1;;
+esac
+
+VERSION=$1
+VER=$(echo "${1}" | cut -b1,3,4)
+
+OCAML_SRC=$(mktemp -d)
+git clone -b "${VERSION}" git://github.com/ocaml/ocaml.git "${OCAML_SRC}"
+
+rm -rf "upstream/ocaml_${VER}/"
+mkdir "upstream/ocaml_${VER}"
+
+cp -r "${OCAML_SRC}"/file_formats "upstream/ocaml_${VER}/"
+cp -r "${OCAML_SRC}"/parsing "upstream/ocaml_${VER}/"
+cp -r "${OCAML_SRC}"/typing "upstream/ocaml_${VER}/"
+cp -r "${OCAML_SRC}"/utils "upstream/ocaml_${VER}/"
+
+git -C "${OCAML_SRC}" show-ref --hash "refs/remotes/origin/${VERSION}" > "upstream/ocaml_${VER}/base-rev.txt"
+
+echo 'Done.'
