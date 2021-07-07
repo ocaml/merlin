ddc7e3ad0cf358360d31db808163cf3b835237e8
Kate <kit.ty.kate@disroot.org>
2021-07-07 18:25:17 +0100

    Remove C files from the upstream directory

--- upstream-upgrader.sh
+++ upstream-upgrader.sh
@@ -29,15 +29,16 @@ git clone -b "${VERSION}" git://github.com/ocaml/ocaml.git "${OCAML_SRC}"
 rm -rf "upstream/ocaml_${VER}/"
 mkdir "upstream/ocaml_${VER}"
 
-cp -r "${OCAML_SRC}"/file_formats "upstream/ocaml_${VER}/"
-cp -r "${OCAML_SRC}"/parsing "upstream/ocaml_${VER}/"
-cp -r "${OCAML_SRC}"/typing "upstream/ocaml_${VER}/"
-cp -r "${OCAML_SRC}"/utils "upstream/ocaml_${VER}/"
+cp -r "${OCAML_SRC}/file_formats" "upstream/ocaml_${VER}/"
+cp -r "${OCAML_SRC}/parsing" "upstream/ocaml_${VER}/"
+cp -r "${OCAML_SRC}/typing" "upstream/ocaml_${VER}/"
+cp -r "${OCAML_SRC}/utils" "upstream/ocaml_${VER}/"
 
 rm -f "upstream/ocaml_${VER}"/*/dune
 rm -f "upstream/ocaml_${VER}"/*/Makefile
 rm -f "upstream/ocaml_${VER}"/*/*.adoc
 rm -f "upstream/ocaml_${VER}"/*/*.md
+rm -f "upstream/ocaml_${VER}"/*/*.c
 
 git -C "${OCAML_SRC}" show-ref --hash "refs/remotes/origin/${VERSION}" > "upstream/ocaml_${VER}/base-rev.txt"
 
