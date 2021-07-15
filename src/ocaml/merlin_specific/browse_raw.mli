24fa9a04648df92a1d3770e568762f8cf7db74c0
Thomas Refis <thomas.refis@gmail.com>
2021-07-15 15:00:10 +0200

    Browse_raw.node_paths_full: keep lident alongside the path
    Also: keep more precise locations for record fields.

--- src/ocaml/merlin_specific/browse_raw.mli
+++ src/ocaml/merlin_specific/browse_raw.mli
@@ -91,8 +91,10 @@ type node =
   | Open_declaration         of open_declaration
 
   | Method_call              of expression * meth * Location.t
-  | Record_field             of [ `Expression of expression | `Pattern of pattern ] *
-                                Types.label_description * Location.t
+  | Record_field             of [ `Expression of expression
+                                | `Pattern of pattern ]
+                                * Types.label_description
+                                * Longident.t Location.loc
   | Module_binding_name      of module_binding
   | Module_declaration_name  of module_declaration
   | Module_type_declaration_name of module_type_declaration
