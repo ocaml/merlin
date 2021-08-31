25292eeec47750b778f8054abc51a5de82b44bed
Ulysse Gérard <thevoodoos@gmail.com>
2021-08-31 11:16:12 +0200

    Fix Includemod

--- src/utils/misc.mli
+++ src/utils/misc.mli
@@ -308,3 +308,50 @@ type modname = string
 type crcs = (modname * Digest.t option) list
 
 type alerts = string String.Map.t
+
+(* Color handling *)
+module Color : sig
+  type color =
+    | Black
+    | Red
+    | Green
+    | Yellow
+    | Blue
+    | Magenta
+    | Cyan
+    | White
+  ;;
+
+  type style =
+    | FG of color (* foreground *)
+    | BG of color (* background *)
+    | Bold
+    | Reset
+
+  type Format.stag += Style of style list
+
+  val ansi_of_style_l : style list -> string
+  (* ANSI escape sequence for the given style *)
+
+  type styles = {
+    error: style list;
+    warning: style list;
+    loc: style list;
+  }
+
+  val default_styles: styles
+  val get_styles: unit -> styles
+  val set_styles: styles -> unit
+
+  type setting = Auto | Always | Never
+
+  val default_setting : setting
+
+  val setup : setting option -> unit
+  (* [setup opt] will enable or disable color handling on standard formatters
+     according to the value of color setting [opt].
+     Only the first call to this function has an effect. *)
+
+  val set_color_tag_handling : Format.formatter -> unit
+  (* adds functions to support color tags to the given formatter. *)
+end
