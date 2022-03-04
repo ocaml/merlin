29b47f30016770becc87ff7115a21cf9c516ac5a
Thomas Refis <thomas.refis@gmail.com>
2022-03-04 15:41:37 +0100

    upstream: removing old versions directories
    Leaving 4.13 in case we need to diff.

--- upstream/ocaml_411/utils/int_replace_polymorphic_compare.ml
+++
@@ -1,8 +0,0 @@
-let ( = )   : int -> int -> bool = Stdlib.( = )
-let ( <> )  : int -> int -> bool = Stdlib.( <> )
-let ( < )   : int -> int -> bool = Stdlib.( < )
-let ( > )   : int -> int -> bool = Stdlib.( > )
-let ( <= )  : int -> int -> bool = Stdlib.( <= )
-let ( >= )  : int -> int -> bool = Stdlib.( >= )
-
-let compare : int -> int -> int  = Stdlib.compare
