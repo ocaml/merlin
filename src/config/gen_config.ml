d5b9598b656109acda9b528e00bfd0751b71f9da
Ulysse Gérard <thevoodoos@gmail.com>
2022-03-11 12:12:51 +0100

    Add OCaml 5.0.0 version to gen_config

--- src/config/gen_config.ml
+++ src/config/gen_config.ml
@@ -17,5 +17,5 @@ let ocamlversion :
   | `OCaml_4_03_0 | `OCaml_4_04_0 | `OCaml_4_05_0 | `OCaml_4_06_0
   | `OCaml_4_07_0 | `OCaml_4_07_1 | `OCaml_4_08_0 | `OCaml_4_09_0
   | `OCaml_4_10_0 | `OCaml_4_11_0 | `OCaml_4_12_0 | `OCaml_4_13_0
-  | `OCaml_4_14_0 ] = %s
+  | `OCaml_4_14_0 | `OCaml_5_0_0 ] = %s
 |} ocaml_version_val
