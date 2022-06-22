e0514d2c725c52a85a0f5759f1a0cb2f56daf12c
Ulysse Gérard <thevoodoos@gmail.com>
2022-06-22 15:27:08 +0200

    Fix type-enclosing deduplication

--- src/frontend/query_commands.ml
+++ src/frontend/query_commands.ml
@@ -314,20 +314,8 @@ let dispatch pipeline (type a) : a Query_protocol.t -> a =
           small_enclosings
       );
 
-    let all_items =
-      let normalize ({Location. loc_start; loc_end; _}, text, _tail) =
-        Lexing.split_pos loc_start, Lexing.split_pos loc_end, text in
-      List.merge_cons
-        ~f:(fun a b ->
-            (* Tail position is computed only on result, and result comes last
-               As an approximation, when two items are similar, we returns the
-               rightmost one *)
-            if compare (normalize a) (normalize b) = 0 then Some b else None)
-
-        (small_enclosings @ result)
-    in
     let ppf = Format.str_formatter in
-    List.mapi all_items
+    let all_results = List.mapi (small_enclosings @ result)
       ~f:(fun i (loc,text,tail) ->
           let print = match index with None -> true | Some index -> index = i in
           let ret x = (loc, x, tail) in
@@ -347,6 +335,17 @@ let dispatch pipeline (type a) : a Query_protocol.t -> a =
             ret (`String (Format.flush_str_formatter ()))
           | _ -> ret (`Index i)
         )
+    in
+    let normalize ({Location. loc_start; loc_end; _}, text, _tail) =
+      Lexing.split_pos loc_start, Lexing.split_pos loc_end, text
+    in
+    (* We remove duplicates from the list. Duplicates can appear when the type
+       from the reconstructed identifier is the same as the one stored in the
+       typedtree *)
+    List.merge_cons
+      ~f:(fun a b ->
+          if compare (normalize a) (normalize b) = 0 then Some b else None)
+      all_results
 
   | Enclosing pos ->
     let typer = Mpipeline.typer_result pipeline in
