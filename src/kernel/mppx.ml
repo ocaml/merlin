96abad108ab6090b92bcbf98ac3698ebcae75235
Ulysse Gérard <thevoodoos@gmail.com>
2022-11-03 17:18:39 +0100

    fix: stop using chdir for ppx
    Co-authored-by: Rudi Grinberg <me@rgrinberg.com>

--- src/kernel/mppx.ml
+++ src/kernel/mppx.ml
@@ -2,16 +2,6 @@ open Mconfig
 
 let {Logger. log} = Logger.for_section "Mppx"
 
-let change_directory dir =
-  log ~title:"changing_directory" "%s" dir;
-  match Sys.chdir dir with
-  | () -> true
-  | exception exn ->
-    log ~title:"changing directory"
-      "change_directory %S failed with %a" dir Logger.exn exn;
-    false
-
-
 let with_include_dir path f =
   let saved = !Clflags.include_dirs in
   let restore () = Clflags.include_dirs := saved in
@@ -32,19 +22,12 @@ let with_include_dir path f =
 
 let rewrite cfg parsetree =
   let ppx = cfg.ocaml.ppx in
-  let prev_dir = Sys.getcwd () in
-  let restore () =
-    if not (change_directory prev_dir) then
-      ignore (change_directory "/")
-  in
   (* add include path attribute to the parsetree *)
   with_include_dir (Mconfig.build_path cfg) @@ fun () ->
   match
     Pparse.apply_rewriters ~restore:false ~ppx ~tool_name:"merlin" parsetree
   with
-  | parsetree ->
-    restore ();
-    parsetree
+  | parsetree -> parsetree
   | exception exn ->
     log ~title:"rewrite" "failed with %a" Logger.fmt (fun fmt ->
       match Location.error_of_exn exn with
@@ -54,5 +37,4 @@ let rewrite cfg parsetree =
         Location.print_main fmt err
     );
     Msupport.raise_error exn;
-    restore ();
     parsetree
