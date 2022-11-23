d96f2114134771021dab3832051e045ca4760cc7
Ulysse Gérard <thevoodoos@gmail.com>
2022-11-23 16:34:12 +0100

    Move configuration of the Json pretifier to the new `lib_config` module.

--- src/utils/lib_config.mli
+++ src/utils/lib_config.mli
@@ -9,3 +9,9 @@ val set_program_name : string -> unit
     [set_program]. Defaults to "Merlin". *)
 val program_name : unit -> string
 
+module Json : sig
+    (** Merlin's logger requires a Json pretty-printer for correct operation.
+        [set_pretty_to_string] can be used to provide one. A common pretifier
+        is [Yojson.Basic.pretty_to_string].  *)
+    val set_pretty_to_string : (Std.json -> string) -> unit
+end
