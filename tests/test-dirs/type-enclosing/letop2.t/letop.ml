b3f4096c4faec5d0faf0eb2294c225e102ea114a
Ulysse Gérard <thevoodoos@gmail.com>
2023-07-27 16:02:08 +0200

    Update test with more branches

--- tests/test-dirs/type-enclosing/letop2.t/letop.ml
+++ tests/test-dirs/type-enclosing/letop2.t/letop.ml
@@ -6,5 +6,6 @@ let (and+) x y =
 
 let minus_three (tbl1, tbl2) (key1, key2) =
   let+ foo = Hashtbl.find_opt tbl1 key1
-  and+ bar = Hashtbl.find_opt tbl2 key2 in
-  foo + bar - 3
+  and+ bar = Hashtbl.find_opt tbl2 key2
+  and+ man = Hashtbl.find_opt tbl2 key2 in
+  foo + bar - man
