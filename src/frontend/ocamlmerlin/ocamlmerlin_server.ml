4b4d637940f1bef6be9c7719f1436f5b30df7d62
Liam Stevenson <lstevenson@janestreet.com>
2024-04-18 18:10:37 +0100

    Use set_signal to avoid ignore

--- src/frontend/ocamlmerlin/ocamlmerlin_server.ml
+++ src/frontend/ocamlmerlin/ocamlmerlin_server.ml
@@ -65,7 +65,7 @@ module Server = struct
       Logger.log ~section:"server" ~title:"cannot setup listener" ""
     | Some server ->
       (* If the client closes its connection, don't let it kill us with a SIGPIPE. *)
-      if Sys.unix then ignore @@ Sys.signal Sys.sigpipe Sys.Signal_ignore;
+      if Sys.unix then Sys.set_signal Sys.sigpipe Sys.Signal_ignore;
       loop (File_id.get Sys.executable_name) server;
       Os_ipc.server_close server
 end
