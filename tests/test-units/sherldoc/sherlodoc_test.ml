e4ba8038d0791bb9be84b956dee913208538de14
xvw <xavier.vdw@gmail.com>
2024-09-09 17:12:14 +0200

    Introduce unit-tests (sherlodoc)
    Currently, Merlin's code base only uses test CRAMs, although some
    functions are easier to maintain if they are unit-tested. It is
    therefore a ‘gradual’ introduction of unit tests into the code base.

---
+++ tests/test-units/sherldoc/sherlodoc_test.ml
@@ -0,0 +1,8 @@
+let () =
+  Alcotest.run "merlin-lib.sherlodoc"
+    [
+      Type_expr_test.cases;
+      Name_cost_test.cases;
+      Type_distance_test.cases;
+      Query_parser_test.cases;
+    ]
