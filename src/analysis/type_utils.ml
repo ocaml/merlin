c9da4229743345f7cccd683b5fb05bbb647753b8
Ulysse Gérard <thevoodoos@gmail.com>
2024-09-25 12:54:44 +0200

    [B] #1811 Exposes some helper for reducing direct typedtree usage in Lsp

--- src/analysis/type_utils.ml
+++ src/analysis/type_utils.ml
@@ -371,3 +371,4 @@ let is_deprecated =
            ("deprecated" | "ocaml.deprecated"); loc = _}, _ ->
         true
       | _ -> false)
+
---
+++ src/analysis/typedtree_utils.mli
@@ -0,0 +1,23 @@
+(** Utilities to keep explicit Typedtree manipulations local to Merlin_lib
+    for alternative clients like [ocaml-lsp]. *)
+
+(** [extract_toplevel_identifier sigitem] extracts toplevel identifier of
+    a signature item. It returns a list for dealing with recursive elements. *)
+val extract_toplevel_identifier : Typedtree.signature_item -> Ident.t list
+
+(** [let_bound_vars binding_list] extract the [Ident.t] and the
+    location of variables bind in the form of [let b = e ...] in a
+    list of bindings. *)
+val let_bound_vars :
+  Typedtree.value_binding list -> (Ident.t * string Location.loc) list
+
+(** [pat_var_id_and_loc] try to extract the [id] and the [location] of
+    pattern variable. *)
+val pat_var_id_and_loc :
+  Typedtree.pattern -> (Ident.t * string Location.loc) option
+
+(** [pat_alias_id_and_loc] try to extract the [id] and the [location]
+    of pattern alias. *)
+val pat_alias_pat_id_and_loc
+  : Typedtree.pattern
+  -> (Typedtree.pattern * Ident.t * string Location.loc) option
