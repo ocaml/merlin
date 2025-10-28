e2cad11a7641384df034166a0f4fd791222cba4e
Ulysse Gérard <thevoodoos@gmail.com>
2025-10-28 11:08:32 +0100

    Upgrade ocamlformat to 0.28.1

--- src/commands/query_json.ml
+++ src/commands/query_json.ml
@@ -398,8 +398,7 @@ let json_of_locate_types (resp : Locate_types_result.t) =
     `List (`String variant_name :: payload)
   in
   let json_of_node_data :
-      Locate_types_result.(type_ref_payload Tree.node_data) ->
-      _ = function
+      Locate_types_result.(type_ref_payload Tree.node_data) -> _ = function
     | Arrow -> `List [ `String "Arrow" ]
     | Tuple -> `List [ `String "Tuple" ]
     | Object -> `List [ `String "Object" ]
