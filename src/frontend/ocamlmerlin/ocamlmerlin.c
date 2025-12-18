ocaml/merlin

commit cc4596fda397d627c352193e568d8c9a8198448c

ocamlmerlin.c: use the cool _Countof op from C2y
to count the number of elements in an array.

See also:
- https://en.cppreference.com/w/c/preprocessor/include.html
- https://manpages.debian.org/testing/manpages-dev/countof.3.en.html

src/frontend/ocamlmerlin/ocamlmerlin.c
@@ -54,6 +54,24 @@ typedef SSIZE_T ssize_t;
 #define ATLEAST
 #endif
 
+#ifndef __has_feature
+#define __has_feature(x) 0
+#endif
+#ifndef __has_extension
+#define __has_extension __has_feature
+#endif
+#ifndef __has_include
+#define __has_include(x) 0
+#endif
+
+#if __has_include("<stdcountof.h>")
+#include <stdcountof.h>
+#elif __has_extension(c_countof)
+#define countof _Countof
+#else
+#define countof(a) (sizeof(a)/sizeof(*(a)))
+#endif
+
 /** Portability information **/
 
 /* Determine OS, http://stackoverflow.com/questions/6649936
@@ -125,9 +143,9 @@ static const char *path_socketdir(void)
       GetProcAddress(GetModuleHandle("KERNEL32.DLL"), "GetTempPath2A");
     DWORD rc;
     if (pGetTempPath2A != NULL)
-      rc = pGetTempPath2A(_countof(dir), dir);
+      rc = pGetTempPath2A(countof(dir), dir);
     else
-      rc = GetTempPathA(_countof(dir), dir);
+      rc = GetTempPathA(countof(dir), dir);
     if (rc == 0)
       failwith("could not get temporary path");
   }
@@ -534,7 +552,7 @@ static void compute_merlinpath(char merlin_path[ATLEAST PATHSZ], const char *arg
   strsz = strlen(merlin_path);
 
   // Append ocamlmerlin-server
-  if (strsz + sizeof(ocamlmerlin_server) + 8 > PATHSZ)
+  if (strsz + countof(ocamlmerlin_server) + 8 > PATHSZ)
     failwith("path is too long");
 
   strcpy(merlin_path + strsz, ocamlmerlin_server);

commit c93fa62a5b3532e1e2234badffd9983a64551173

Fix include directive for stdcountof.h
typo introduced in cc4596fda397d627c352193e568d8c9a8198448c

src/frontend/ocamlmerlin/ocamlmerlin.c
@@ -63,7 +63,7 @@ typedef SSIZE_T ssize_t;
 #define __has_include(x) 0
 #endif
 
-#if __has_include("<stdcountof.h>")
+#if __has_include(<stdcountof.h>)
 #include <stdcountof.h>
 #elif __has_extension(c_countof)
 #define countof _Countof