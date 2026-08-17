415da94c8f8035238ba202e5aac33658fb34711d
PizieDust <playersrebirth@gmail.com>
2026-08-17 03:30:43 +0100

    track current_loc accumulator in expand_node to filter redundant selection ranges

--- src/analysis/enclosing.ml
+++ src/analysis/enclosing.ml
@@ -3,7 +3,36 @@ open Std
 let ( << ) (x : Location.t) (y : Location.t) =
   x.loc_end.pos_cnum <= y.loc_start.pos_cnum
 
-let rec expand_node (nodes : Browse_raw.node list) =
+let before (x : Location.t) (y : Location.t) =
+  x.loc_start.pos_cnum < y.loc_start.pos_cnum
+
+let is_valid_loc (loc : Location.t) =
+  loc.loc_start.pos_cnum >= 0
+  && loc.loc_end.pos_cnum >= 0
+  && loc.loc_start.pos_cnum <= loc.loc_end.pos_cnum
+
+let contains (loc1 : Location.t) (loc2 : Location.t) =
+  loc1.loc_start.pos_cnum <= loc2.loc_start.pos_cnum
+  && loc2.loc_end.pos_cnum <= loc1.loc_end.pos_cnum
+
+let min_pos (pos1 : Lexing.position) (pos2 : Lexing.position) =
+  if pos1.pos_cnum < 0 then pos2
+  else if pos2.pos_cnum < 0 then pos1
+  else if pos1.pos_cnum < pos2.pos_cnum then pos1
+  else pos2
+
+let max_pos (pos1 : Lexing.position) (pos2 : Lexing.position) =
+  if pos1.pos_cnum < 0 then pos2
+  else if pos2.pos_cnum < 0 then pos1
+  else if pos1.pos_cnum < pos2.pos_cnum then pos2
+  else pos1
+
+let merge (loc1 : Location.t) (loc2 : Location.t) =
+  let loc_start = min_pos loc1.loc_start loc2.loc_start in
+  let loc_end = max_pos loc1.loc_end loc2.loc_end in
+  { Location.loc_start; loc_end; loc_ghost = false }
+
+let rec expand_node ~current_loc (nodes : Browse_raw.node list) =
   let get_binding_loc vb exp =
     let vb_loc = Mbrowse.node_loc (Value_binding vb) in
     let body_exp = Browse_raw.node_merlin_loc Location.none (Expression exp) in
@@ -13,10 +42,33 @@ let rec expand_node (nodes : Browse_raw.node list) =
   | [] -> []
   | Expression { exp_desc = Texp_let (_, first_vb :: _, exp); _ } :: q ->
     let loc = get_binding_loc first_vb exp in
-    loc :: expand_node (Expression exp :: q)
+    let body_loc = Browse_raw.node_merlin_loc Location.none (Expression exp) in
+    if contains current_loc body_loc then
+      if is_valid_loc loc then
+        if contains current_loc loc then
+          expand_node ~current_loc q
+        else
+          let new_loc = merge current_loc loc in
+          loc :: expand_node ~current_loc:new_loc q
+      else
+        expand_node ~current_loc q
+    else
+      if is_valid_loc loc then
+        let new_loc = merge current_loc loc in
+        loc :: expand_node ~current_loc:new_loc (Expression exp :: q)
+      else
+        expand_node ~current_loc (Expression exp :: q)
   | Expression { exp_desc = Texp_sequence (exp1, exp2); _ } :: q ->
     let exp1_loc = Browse_raw.node_merlin_loc Location.none (Expression exp1) in
-    exp1_loc :: expand_node (Expression exp2 :: q)
+    let exp2_loc = Browse_raw.node_merlin_loc Location.none (Expression exp2) in
+    if contains current_loc exp2_loc then
+      expand_node ~current_loc q
+    else
+      if is_valid_loc exp1_loc then
+        let new_loc = merge current_loc exp1_loc in
+        exp1_loc :: expand_node ~current_loc:new_loc (Expression exp2 :: q)
+      else
+        expand_node ~current_loc (Expression exp2 :: q)
   | (Expression
        { exp_desc =
            Texp_apply
@@ -34,12 +86,26 @@ let rec expand_node (nodes : Browse_raw.node list) =
          _
        } as node)
     :: q
-    when pat.pat_loc << value_binded.exp_loc
-         && value_binded.exp_loc << body.exp_loc ->
-    let body_exp = Browse_raw.node_merlin_loc Location.none (Expression body) in
+    when before pat.pat_loc value_binded.exp_loc
+         && before value_binded.exp_loc body.exp_loc ->
+    let body_loc = Browse_raw.node_merlin_loc Location.none (Expression body) in
     let vb_loc = Mbrowse.node_loc node in
-    let loc = { vb_loc with loc_end = body_exp.loc_start } in
-    loc :: expand_node (Expression body :: q)
+    let loc = { vb_loc with loc_end = body_loc.loc_start } in
+    if contains current_loc body_loc then
+      if is_valid_loc loc then
+        if contains current_loc loc then
+          expand_node ~current_loc q
+        else
+          let new_loc = merge current_loc loc in
+          loc :: expand_node ~current_loc:new_loc q
+      else
+        expand_node ~current_loc q
+    else
+      if is_valid_loc loc then
+        let new_loc = merge current_loc loc in
+        loc :: expand_node ~current_loc:new_loc (Expression body :: q)
+      else
+        expand_node ~current_loc (Expression body :: q)
   | Expression
       { exp_desc =
           Texp_match
@@ -48,36 +114,46 @@ let rec expand_node (nodes : Browse_raw.node list) =
         _
       }
     :: q
-    when c_lhs.pat_loc << exp.exp_loc ->
-    let body_exp =
-      Browse_raw.node_merlin_loc Location.none (Expression c_rhs)
-    in
-    let let_in = { exp_loc with loc_end = body_exp.loc_start } in
-    let_in :: expand_node (Expression c_rhs :: q)
+    when before c_lhs.pat_loc exp.exp_loc ->
+    let body_loc = Browse_raw.node_merlin_loc Location.none (Expression c_rhs) in
+    let let_in = { exp_loc with loc_end = body_loc.loc_start } in
+    if contains current_loc body_loc then
+      if is_valid_loc let_in then
+        if contains current_loc let_in then
+          expand_node ~current_loc q
+        else
+          let new_loc = merge current_loc let_in in
+          let_in :: expand_node ~current_loc:new_loc q
+      else
+        expand_node ~current_loc q
+    else
+      if is_valid_loc let_in then
+        let new_loc = merge current_loc let_in in
+        let_in :: expand_node ~current_loc:new_loc (Expression c_rhs :: q)
+      else
+        expand_node ~current_loc (Expression c_rhs :: q)
   | (Value_binding _ as _vb_node)
     :: (Expression { exp_desc = Texp_let (_, first_vb :: _, exp); _ } :: _ as q)
     ->
     let loc = get_binding_loc first_vb exp in
-    loc :: expand_node q
-  | node :: q -> Mbrowse.node_loc node :: expand_node q
-
-let min_pos (pos1 : Lexing.position) (pos2 : Lexing.position) =
-  if pos1.pos_cnum < 0 then pos2
-  else if pos2.pos_cnum < 0 then pos1
-  else if pos1.pos_cnum < pos2.pos_cnum then pos1
-  else pos2
-
-let max_pos (pos1 : Lexing.position) (pos2 : Lexing.position) =
-  if pos1.pos_cnum < 0 then pos2
-  else if pos2.pos_cnum < 0 then pos1
-  else if pos1.pos_cnum < pos2.pos_cnum then pos2
-  else pos1
-
-let merge (loc1 : Location.t) (loc2 : Location.t) =
-  let loc_start = min_pos loc1.loc_start loc2.loc_start in
-  let loc_end = max_pos loc1.loc_end loc2.loc_end in
-  (* TODO: deal with loc_ghost *)
-  { loc1 with loc_start; loc_end }
+    if is_valid_loc loc then
+      if contains current_loc loc then
+        expand_node ~current_loc q
+      else
+        let new_loc = merge current_loc loc in
+        loc :: expand_node ~current_loc:new_loc q
+    else
+      expand_node ~current_loc q
+  | node :: q ->
+    let loc = Mbrowse.node_loc node in
+    if is_valid_loc loc then
+      if contains current_loc loc then
+        expand_node ~current_loc q
+      else
+        let new_loc = merge current_loc loc in
+        loc :: expand_node ~current_loc:new_loc q
+    else
+      expand_node ~current_loc q
 
 let make_expand l =
   match l with
@@ -92,15 +168,10 @@ let make_expand l =
 
 let locs (mbrowse : Mbrowse.t) =
   mbrowse |> List.map ~f:snd
-  (* |> List.concat_map ~f:(fun (_env, node) -> *)
-  (*     match node with *)
-  (*     | _ -> [ node ]) *)
-  |> expand_node
+  |> expand_node ~current_loc:Location.none
   |> make_expand
-  |>
-  (* We remove possible duplicates from the list *)
-  List.fold_left ~init:[] ~f:(fun acc loc ->
-      match acc with
-      | hd :: _ as acc when Location_aux.compare hd loc = 0 -> acc
-      | _ -> loc :: acc)
+  |> List.fold_left ~init:[] ~f:(fun acc loc ->
+         match acc with
+         | hd :: _ as acc when Location_aux.compare hd loc = 0 -> acc
+         | _ -> loc :: acc)
   |> List.rev
